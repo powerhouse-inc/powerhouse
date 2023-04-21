cb523e2194492484da88d7e6acea1ccc41d93451
acaldas <acaldas.dev@gmail.com>
2023-04-21 09:32:38 +0100

    Budget statement demo

---
+++ src/budget-statement/editor.tsx
@@ -0,0 +1,152 @@
+import React, { useCallback, useEffect, useState } from "react";
+import {
+    BudgetStatementAction,
+    BudgetStatementDocument,
+    actions,
+    reducer,
+    utils,
+} from "document-model-libs/browser/budget-statement";
+import { BudgetStatement } from "document-model-editors";
+
+export default function Editor() {
+    const [initialBudgetStatement, setInitialBudgetStatement] =
+        useState<BudgetStatementDocument>(utils.createBudgetStatement());
+    const [budgetStatement, setBudgetStatement] =
+        useState<BudgetStatementDocument>();
+
+    useEffect(() => {
+        setBudgetStatement(initialBudgetStatement);
+    }, [initialBudgetStatement]);
+
+    async function loadNode() {
+        // @ts-ignore
+        const budgetStatement = await window.electronAPI.openFile();
+        setInitialBudgetStatement(budgetStatement);
+    }
+
+    async function saveNode() {
+        // @ts-ignore
+        await window.electronAPI.saveFile(budgetStatement);
+    }
+
+    async function loadBrowser() {
+        // open file picker, destructure the one element returned array
+        const [fileHandle] = await window.showOpenFilePicker();
+        const file = await fileHandle.getFile();
+
+        const budgetStatement = await utils.loadBudgetStatementFromInput(file);
+        setInitialBudgetStatement(budgetStatement);
+    }
+
+    async function saveBrowser() {
+        if (!budgetStatement) {
+            return;
+        }
+        // @ts-ignore
+        const file = await window.showSaveFilePicker({
+            suggestedName: `${
+                budgetStatement?.data.month ?? "budget"
+            }.phbs.zip`,
+        });
+
+        utils.saveBudgetStatementToFileHandle(budgetStatement, file);
+    }
+
+    const onChange = useCallback((budgetStatement: BudgetStatementDocument) => {
+        setBudgetStatement({ ...budgetStatement });
+    }, []);
+
+    const newDocument = () => {
+        setInitialBudgetStatement(utils.createBudgetStatement());
+    };
+
+    const operations = budgetStatement
+        ? [...budgetStatement.operations].reverse()
+        : [];
+
+    function undo() {
+        if (!budgetStatement) {
+            return;
+        }
+        const newBudget = reducer(budgetStatement, actions.undo());
+        setInitialBudgetStatement(newBudget);
+    }
+
+    function redo() {
+        if (!budgetStatement) {
+            return;
+        }
+        const newBudget = reducer(budgetStatement, actions.redo());
+        setInitialBudgetStatement(newBudget);
+    }
+
+    const canUndo = budgetStatement && budgetStatement.revision > 0;
+    const canRedo =
+        budgetStatement &&
+        budgetStatement.revision < budgetStatement.operations.length;
+
+    return (
+        <div>
+            <div>
+                <button onClick={newDocument}>New document</button>&ensp;
+                {window.electronAPI ? (
+                    <>
+                        <button onClick={loadNode}>Load document</button>&ensp;
+                        <button onClick={saveNode}>Save document</button>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={loadBrowser}>Load document</button>
+                        &ensp;
+                        <button onClick={saveBrowser}>Save document</button>
+                    </>
+                )}
+                <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <div style={{ width: "50%" }}>
+                        <BudgetStatement.Editor
+                            key={initialBudgetStatement?.revision}
+                            budgetStatement={initialBudgetStatement}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div style={{ width: "40%" }}>
+                        <h3>
+                            Operations&emsp;
+                            <button disabled={!canUndo} onClick={undo}>
+                                Undo
+                            </button>
+                            &ensp;
+                            <button disabled={!canRedo} onClick={redo}>
+                                Redo
+                            </button>
+                        </h3>
+                        <div></div>
+                        <ul>
+                            {operations.map((o) => (
+                                <li
+                                    key={o.index}
+                                    style={{
+                                        opacity:
+                                            budgetStatement &&
+                                            o.index < budgetStatement?.revision
+                                                ? 1
+                                                : 0.5,
+                                    }}
+                                >
+                                    <b>{`${o.index + 1} - ${o.type}`}</b>
+                                    <br />
+                                    <pre style={{ overflow: "auto" }}>
+                                        {JSON.stringify(o.input, null, 2)}
+                                    </pre>
+                                    <hr />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/globals.d.ts
@@ -0,0 +1,12 @@
+import { BrowserWindow } from "electron";
+
+export {};
+
+declare global {
+    interface Window {
+        electronAPI?: any;
+    }
+
+    const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+    const MAIN_WINDOW_VITE_NAME: string;
+}
