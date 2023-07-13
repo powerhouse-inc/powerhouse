8d360d701aed0b98bc31ee833e43b032c0327439
Wouter Kampmann <wouter@ksdc.be>
2023-07-13 12:38:24 +0200

    Vite/Electron build order fix, cfr. https://github.com/electron/forge/issues/3208

--- apps/connect/vite.main.config.mjs
+++ apps/connect/vite.main.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "vite";
+import { restart } from "./vite-fix"
 
 // https://vitejs.dev/config
-export default defineConfig({ build: { minify: false } });
+export default defineConfig({ 
+    build: { minify: false },
+    plugins: [restart()],
+ });
