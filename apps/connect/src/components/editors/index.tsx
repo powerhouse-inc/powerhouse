42f9b43bca247398572d20e4d2ac897c8b3f59ba
acaldas <acaldas.dev@gmail.com>
2023-08-04 14:16:57 +0100

    Revert "Revert "Improved document editors imports""
    This reverts commit 1a398713fe7ed4ec40ef3aa0a01c9286c0c1cb2e.

--- apps/connect/src/components/editors/index.tsx
+++ apps/connect/src/components/editors/index.tsx
@@ -2,7 +2,7 @@ import {
     Action,
     BaseAction,
     Document,
-} from '@acaldas/document-model-libs/document';
+} from '@acaldas/document-model-libs/browser/document';
 
 export interface EditorProps<T = unknown, A extends Action = Action> {
     document?: Document<T, A | BaseAction>;
