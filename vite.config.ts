143ae7a8f2079518ddc552638f4ab0133bf6c34e
acaldas <acaldas@powerhouse.inc>
2023-09-13 15:11:10 +0100

    Simplified exports

--- vite.config.ts
+++ vite.config.ts
@@ -49,6 +49,7 @@ export default defineConfig(({ mode = 'node' }) => {
             rollupOptions: {
                 external,
                 output: {
+                    chunkFileNames: 'internal/[name]-[hash].js',
                     exports: 'named',
                 },
             },
