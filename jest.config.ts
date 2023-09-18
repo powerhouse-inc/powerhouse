1ab654505bda46c10ffd6f22a60d7d591de33cb4
acaldas <acaldas@powerhouse.inc>
2023-09-18 16:49:39 +0100

    Config improvements

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
 import { JestConfigWithTsJest } from 'ts-jest';
+import { getConfig } from '@acaldas/powerhouse';
+
+const { documentModelsDir, editorsDir } = getConfig();
 
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     collectCoverage: true,
     collectCoverageFrom: [
-        'document-models/src/*.ts',
-        'document-models/gen/*.ts',
+        `${documentModelsDir}/**/*.ts`,
+        `${editorsDir}/**/*.ts`,
     ],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
+    },
 } satisfies JestConfigWithTsJest;
