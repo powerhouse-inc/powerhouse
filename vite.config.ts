4b18956281ca57068b58f9cdba1af58ae0682794
acaldas <acaldas@powerhouse.inc>
2023-10-13 13:34:49 +0100

    Added initial bundle setup

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        libInjectCss(),
+        dts({
+            include: ['src'],
+            exclude: ['src/stories'],
+        }),
+    ],
+    build: {
+        cssCodeSplit: true,
+        sourcemap: true,
+        rollupOptions: {
+            external: [
+                ...Object.keys(pkg.peerDependencies),
+                'react/jsx-runtime',
+            ],
+            output: {
+                // preserveModules: true,
+            },
+        },
+        lib: {
+            entry: [resolve('src', 'index.ts')],
+            formats: ['es'],
+            fileName(_format, entryName) {
+                return `${entryName}.js`;
+            },
+        },
+    },
+});
