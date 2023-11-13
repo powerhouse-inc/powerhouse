a5b1ddd96fd0131f752403aebf4652846b2536da
ryanwolhuter <dev@ryanwolhuter.com>
2023-11-13 10:02:41 +0200

    feat: add breadcrumbs component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/connect/components/breadcrumbs/breadcrumbs.tsx
+++ src/connect/components/breadcrumbs/breadcrumbs.tsx
@@ -1,28 +1,69 @@
 import { TreeItem } from '..';
 
-export type BreadcrumbsProps<TItemId extends string> =
+function findDeepestSelectedPath<T extends string = string>(root: TreeItem<T>) {
+    let deepestPath: TreeItem<T>[] = [];
+    const currentDepth = 0;
+    let maxDepth = -1;
+
+    function dfs(node: TreeItem<T>, path: TreeItem<T>[], depth: number) {
+        // Add the current node to the path
+        path.push(node);
+
+        // If this node is selected and deeper than the previous found, update the deepest path
+        if (node.isSelected && depth > maxDepth) {
+            maxDepth = depth;
+            deepestPath = path.slice(); // Copy the current path
+        }
+
+        // Continue to child nodes
+        node.children?.forEach(child => {
+            dfs(child, path.slice(), depth + 1); // Use a copy of the path for each child
+        });
+    }
+
+    // Start DFS from the root node
+    dfs(root, [], currentDepth);
+
+    return deepestPath;
+}
+
+export type BreadcrumbsProps<T extends string> =
     React.HTMLAttributes<HTMLDivElement> & {
-        items: TreeItem<TItemId>;
+        rootItem: TreeItem<T>;
         onItemClick?: (
             event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-            item: TreeItem<TItemId>,
+            item: TreeItem<T>,
         ) => void;
     };
 
-export function Breadcrumbs<TItemId extends string>(
-    props: BreadcrumbsProps<TItemId>,
+export function Breadcrumbs<T extends string = string>(
+    props: BreadcrumbsProps<T>,
 ) {
-    return <div></div>;
+    const breadcrumbItems = findDeepestSelectedPath(props.rootItem);
+    return breadcrumbItems.map(item => (
+        <Breadcrumb
+            key={item.id}
+            item={item}
+            onClick={e => props.onItemClick?.(e, item)}
+        />
+    ));
 }
 
-export type BreadcrumbProps = {
+export type BreadcrumbProps<T extends string> = {
     onClick?: (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>,
         item: TreeItem<string>,
     ) => void;
-    item: TreeItem<string>;
+    item: TreeItem<T>;
 };
 
-export function Breadcrumb(props: BreadcrumbProps) {
-    return <div></div>;
+export function Breadcrumb<T extends string>(props: BreadcrumbProps<T>) {
+    return (
+        <>
+            <div role="button" onClick={e => props.onClick?.(e, props.item)}>
+                {props.item.label}
+            </div>
+            /
+        </>
+    );
 }
