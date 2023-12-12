b6e0d8de0aad9a050cd594a8ed966aadea89536a
Guillermo Puente <guillermo.ps09@gmail.com>
2023-12-12 19:24:57 -0300

    feat: added support for item options in FileItem and FolderItem

--- src/connect/components/file-item/file-item.tsx
+++ src/connect/components/file-item/file-item.tsx
@@ -3,8 +3,13 @@ import GlobalImg from '@/assets/icons/global.png';
 import LegalImg from '@/assets/icons/legal.png';
 import ProfileImg from '@/assets/icons/profile.png';
 import TemplateImg from '@/assets/icons/template.png';
+import {
+    ConnectDropdownMenu,
+    ConnectDropdownMenuProps,
+    defaultDropdownMenuOptions,
+} from '@/connect';
 import { DivProps, Icon } from '@/powerhouse';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 const iconMap = {
@@ -20,19 +25,24 @@ export type FileItemIconType = keyof typeof iconMap;
 export interface FileItemProps extends DivProps {
     title: string;
     subTitle?: string;
-    onOptionsClick?: DivProps['onClick'];
     icon?: FileItemIconType;
     customIcon?: React.ReactNode;
+    itemOptions?: ConnectDropdownMenuProps['items'];
+    onOptionsClick: ConnectDropdownMenuProps['onItemClick'];
 }
 
 export const FileItem: React.FC<FileItemProps> = ({
     title,
     subTitle,
     customIcon,
+    itemOptions,
     onOptionsClick,
     icon = 'global',
     ...divProps
 }) => {
+    const containerRef = useRef(null);
+    const [isDropdownMenuOpen, setIsDropdownMenuOpen] = useState(false);
+
     const containerStyles = twMerge(
         'group flex h-12 cursor-pointer select-none items-center rounded-lg bg-gray-200 px-2 text-gray-600 hover:text-gray-800',
         divProps.className,
@@ -49,30 +59,45 @@ export const FileItem: React.FC<FileItemProps> = ({
     );
 
     return (
-        <div {...divProps} className={containerStyles}>
-            <div className="relative flex flex-1 flex-row items-center overflow-hidden">
-                <div className="mr-1.5">{iconNode}</div>
-                <div className="overflow-hidden text-gray-800">
-                    <div className="max-h-6 truncate text-sm font-medium">
-                        {title}
-                    </div>
-                    <div className="max-h-6 truncate text-xs font-medium text-gray-600">
-                        {subTitle}
+        <div className="relative" ref={containerRef}>
+            <div {...divProps} className={containerStyles}>
+                <div className="relative flex flex-1 flex-row items-center overflow-hidden">
+                    <div className="mr-1.5">{iconNode}</div>
+                    <div className="overflow-hidden text-gray-800">
+                        <div className="max-h-6 truncate text-sm font-medium">
+                            {title}
+                        </div>
+                        <div className="max-h-6 truncate text-xs font-medium text-gray-600">
+                            {subTitle}
+                        </div>
                     </div>
                 </div>
+                <div
+                    onClick={e => {
+                        e.stopPropagation();
+                        setIsDropdownMenuOpen(true);
+                    }}
+                >
+                    <Icon
+                        name="vertical-dots"
+                        className="hidden group-hover:inline-block"
+                        size={24}
+                    />
+                </div>
             </div>
-            <div
-                onClick={e => {
-                    e.stopPropagation();
-                    onOptionsClick?.(e);
+            <ConnectDropdownMenu
+                isOpen={isDropdownMenuOpen}
+                onOpenChange={() => setIsDropdownMenuOpen(!isDropdownMenuOpen)}
+                items={itemOptions || [...defaultDropdownMenuOptions]}
+                menuClassName="bg-white cursor-pointer"
+                menuItemClassName="hover:bg-slate-50 px-2"
+                onItemClick={onOptionsClick}
+                popoverProps={{
+                    triggerRef: containerRef,
+                    placement: 'bottom end',
+                    offset: -10,
                 }}
-            >
-                <Icon
-                    name="vertical-dots"
-                    className="hidden group-hover:inline-block"
-                    size={24}
-                />
-            </div>
+            />
         </div>
     );
 };
--- src/connect/components/folder-item/folder-item.tsx
+++ src/connect/components/folder-item/folder-item.tsx
@@ -1,45 +1,70 @@
+import {
+    ConnectDropdownMenu,
+    ConnectDropdownMenuProps,
+    defaultDropdownMenuOptions,
+} from '@/connect';
 import { DivProps, Icon } from '@/powerhouse';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export interface FolderItemProps extends DivProps {
     title: string;
-    onOptionsClick?: DivProps['onClick'];
+    itemOptions?: ConnectDropdownMenuProps['items'];
+    onOptionsClick: ConnectDropdownMenuProps['onItemClick'];
 }
 
 export const FolderItem: React.FC<FolderItemProps> = ({
     title,
+    itemOptions,
     onOptionsClick,
     ...divProps
 }) => {
+    const containerRef = useRef(null);
+    const [isDropdownMenuOpen, setIsDropdownMenuOpen] = useState(false);
+
     const containerStyles = twMerge(
         'group flex h-12 cursor-pointer select-none items-center rounded-lg bg-gray-200 px-2 text-gray-600 hover:text-gray-800',
         divProps.className,
     );
 
     return (
-        <div {...divProps} className={containerStyles}>
-            <div className="relative flex flex-1 flex-row items-center overflow-hidden">
-                <div className="p-1">
-                    <Icon name="folder-close" size={24} />
+        <div className="relative" ref={containerRef}>
+            <div {...divProps} className={containerStyles}>
+                <div className="relative flex flex-1 flex-row items-center overflow-hidden">
+                    <div className="p-1">
+                        <Icon name="folder-close" size={24} />
+                    </div>
+                    <div className="ml-3 max-h-6 overflow-hidden whitespace-nowrap font-medium text-slate-200 group-hover:text-gray-800">
+                        {title}
+                    </div>
+                    <div className="absolute right-0 h-full w-12 bg-gradient-to-r from-transparent to-gray-200" />
                 </div>
-                <div className="ml-3 max-h-6 overflow-hidden whitespace-nowrap font-medium text-slate-200 group-hover:text-gray-800">
-                    {title}
+                <div
+                    onClick={e => {
+                        e.stopPropagation();
+                        setIsDropdownMenuOpen(true);
+                    }}
+                >
+                    <Icon
+                        name="vertical-dots"
+                        className="hidden group-hover:inline-block"
+                        size={24}
+                    />
                 </div>
-                <div className="absolute right-0 h-full w-12 bg-gradient-to-r from-transparent to-gray-200" />
             </div>
-            <div
-                onClick={e => {
-                    e.stopPropagation();
-                    onOptionsClick?.(e);
+            <ConnectDropdownMenu
+                isOpen={isDropdownMenuOpen}
+                onOpenChange={() => setIsDropdownMenuOpen(!isDropdownMenuOpen)}
+                items={itemOptions || [...defaultDropdownMenuOptions]}
+                menuClassName="bg-white cursor-pointer"
+                menuItemClassName="hover:bg-slate-50 px-2"
+                onItemClick={onOptionsClick}
+                popoverProps={{
+                    triggerRef: containerRef,
+                    placement: 'bottom end',
+                    offset: -10,
                 }}
-            >
-                <Icon
-                    name="vertical-dots"
-                    className="hidden group-hover:inline-block"
-                    size={24}
-                />
-            </div>
+            />
         </div>
     );
 };
