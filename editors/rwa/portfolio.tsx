a0941ac16b22b05b950a812db8fc79c8bdb2944c
Ryan Wolhuter <dev@ryanwolhuter.com>
2024-02-16 13:38:23 +0200

    feat: handle skew icon and move table components to design system (#79)
    * fix: skew icon
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * feat: move create new button to design system
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * feat: bump design system
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    ---------
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- editors/rwa/portfolio.tsx
+++ editors/rwa/portfolio.tsx
@@ -1,12 +1,9 @@
 import {
     FixedIncomeAsset,
     FixedIncomeAssetsTableProps,
-    Icon,
-    RWAAssetDetails,
     RWAFixedIncomeAssetsTable,
 } from '@powerhousedao/design-system';
 import { RWAAssetDetailInputs } from '@powerhousedao/design-system/dist/rwa/components/asset-details/form';
-import { addDays } from 'date-fns';
 import { utils } from 'document-model/document';
 import { useCallback, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
@@ -16,7 +13,6 @@ import {
     getDifferences,
     isFixedIncomeAsset,
 } from '../../document-models/real-world-assets';
-import { mockFixedIncomeTypes, mockSpvs } from './assets-mock-data';
 import { IProps } from './editor';
 
 const fieldsPriority: (keyof FixedIncomeAsset)[] = [
@@ -74,7 +70,7 @@ export const Portfolio = (props: IProps) => {
             setSelectedAssetToEdit(undefined);
         }, []);
 
-    const onSubmitEdit: FixedIncomeAssetsTableProps['onSubmitForm'] =
+    const onSubmitEdit: FixedIncomeAssetsTableProps['onSubmitEdit'] =
         useCallback(
             data => {
                 if (!selectedAssetToEdit) return;
@@ -100,7 +96,7 @@ export const Portfolio = (props: IProps) => {
             [dispatch, selectedAssetToEdit],
         );
 
-    const onSubmitCreate: FixedIncomeAssetsTableProps['onSubmitForm'] =
+    const onSubmitCreate: FixedIncomeAssetsTableProps['onSubmitCreate'] =
         useCallback(
             data => {
                 const asset = createAssetFromFormInputs(data);
@@ -122,65 +118,27 @@ export const Portfolio = (props: IProps) => {
                 Details on the distribution of assets among different financial
                 institutions or investment vehicles.
             </p>
-            <div>
-                <RWAFixedIncomeAssetsTable
-                    className={twMerge(
-                        'bg-white',
-                        expandedRowId && 'max-h-[680px]',
-                    )}
-                    items={portfolio}
-                    fixedIncomeTypes={fixedIncomeTypes}
-                    spvs={spvs}
-                    fieldsPriority={fieldsPriority}
-                    columnCountByTableWidth={columnCountByTableWidth}
-                    expandedRowId={expandedRowId}
-                    selectedAssetToEdit={selectedAssetToEdit}
-                    toggleExpandedRow={toggleExpandedRow}
-                    onClickDetails={onClickDetails}
-                    setSelectedAssetToEdit={setSelectedAssetToEdit}
-                    onCancelEdit={onCancelEdit}
-                    onSubmitForm={onSubmitEdit}
-                    footer={
-                        <button
-                            onClick={() => setShowNewAssetForm(true)}
-                            className="flex h-[42px] text-gray-900 text-sm font-semibold justify-center items-center w-full bg-white gap-x-2"
-                        >
-                            <span>Create Asset</span>
-                            <Icon name="plus" size={14} />
-                        </button>
-                    }
-                />
-            </div>
-            {showNewAssetForm && (
-                <div className="bg-white mt-4 rounded-md border border-gray-300">
-                    <RWAAssetDetails
-                        asset={{
-                            id: '',
-                            name: '',
-                            fixedIncomeTypeId: fixedIncomeTypes[0].id,
-                            spvId: spvs[0].id,
-                            maturity: addDays(new Date(), 30)
-                                .toISOString()
-                                .split('T')[0],
-                            notional: 0,
-                            coupon: 0,
-                            purchasePrice: 0,
-                            purchaseDate: '',
-                            totalDiscount: 0,
-                            purchaseProceeds: 0,
-                            annualizedYield: 0,
-                        }}
-                        mode="edit"
-                        operation="create"
-                        fixedIncomeTypes={mockFixedIncomeTypes}
-                        spvs={mockSpvs}
-                        onClose={() => setShowNewAssetForm(false)}
-                        onCancel={() => setShowNewAssetForm(false)}
-                        onSubmitForm={onSubmitCreate}
-                        hideNonEditableFields
-                    />
-                </div>
-            )}
+            <RWAFixedIncomeAssetsTable
+                className={twMerge(
+                    'bg-white',
+                    expandedRowId && 'max-h-[680px]',
+                )}
+                items={portfolio}
+                fixedIncomeTypes={fixedIncomeTypes}
+                spvs={spvs}
+                fieldsPriority={fieldsPriority}
+                columnCountByTableWidth={columnCountByTableWidth}
+                expandedRowId={expandedRowId}
+                selectedAssetToEdit={selectedAssetToEdit}
+                showNewAssetForm={showNewAssetForm}
+                toggleExpandedRow={toggleExpandedRow}
+                onClickDetails={onClickDetails}
+                setSelectedAssetToEdit={setSelectedAssetToEdit}
+                setShowNewAssetForm={setShowNewAssetForm}
+                onCancelEdit={onCancelEdit}
+                onSubmitEdit={onSubmitEdit}
+                onSubmitCreate={onSubmitCreate}
+            />
         </div>
     );
 };
