00ea5ae10e6f7e50ab6488b0da94f5f389cc13f5
Guillermo Puente <guillermo.ps09@gmail.com>
2024-03-26 16:56:34 -0300

    feat: removed electron-deeplink pkg and updated deep links

--- apps/connect/src/app/deeplink.ts
+++ apps/connect/src/app/deeplink.ts
@@ -1,7 +1,5 @@
-// TODO: electron-deeplink dep is deprecated, we need to find a new alternative
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-const { Deeplink } = require('electron-deeplink');
-import isDev from 'electron-is-dev';
+import path from 'path';
 
 export function addDeeplink(
     app: Electron.App,
@@ -9,16 +7,15 @@ export function addDeeplink(
     protocol: string,
     handleOpenUrl: (event: Electron.Event, url: string) => void,
 ) {
-    app.on('ready', () => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        new Deeplink({
-            app,
-            protocol: protocol,
-            isDev,
-            debugLogging: true,
-            mainWindow: window,
-        });
-    });
+    if (process.defaultApp) {
+        if (process.argv.length >= 2) {
+            app.setAsDefaultProtocolClient(protocol, process.execPath, [
+                path.resolve(process.argv[1]),
+            ]);
+        }
+    } else {
+        app.setAsDefaultProtocolClient(protocol);
+    }
 
     app.on('open-url', (event, url) => {
         if (url.startsWith(`${protocol}://`)) {
