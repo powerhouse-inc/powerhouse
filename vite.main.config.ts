645b6c0f3f3e40eec861d9cf1cab8378cd83a13b
Frank <frank.pfeift@protonmail.com>
2024-04-09 10:47:11 +0200

    chore: readd rewrites

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-
+const ASSET_URL = process.env.ASSET_URL || '';
 // https://vitejs.dev/config
 export default defineConfig({
     build: { minify: false, rollupOptions: { external: [''] } },
+    base: `${ASSET_URL}/dist/`,
     resolve: {
         alias: {
             src: path.resolve(__dirname, './src'),
