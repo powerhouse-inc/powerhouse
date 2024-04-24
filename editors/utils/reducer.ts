abbb15b9e16b916e06e9bb22bb389b385a158e95
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2024-04-24 13:13:52 -0400

    feat: re-enable onErrorCallback with new operations error prop (#145)
    * feat: re-enable onErrorCallback with new operations error prop
    
    * feat: re-use same error object

--- editors/utils/reducer.ts
+++ editors/utils/reducer.ts
@@ -49,6 +49,16 @@ export function useDocumentReducer<State, A extends Action, LocalState>(
         setState(_state => {
             try {
                 const newState = reducer(_state, action);
+
+                const operation =
+                    newState.operations[action.scope].slice(-1)[0];
+
+                if (operation.error) {
+                    const error = new Error(operation.error);
+                    onError?.(error);
+                    onErrorCallback?.(error);
+                }
+
                 return newState;
             } catch (error) {
                 onError?.(error);
