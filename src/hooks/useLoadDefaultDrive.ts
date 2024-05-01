fc10ee8428d9d9c24c6cef38cd104ac5f79d759c
acaldas <acaldas.dev@gmail.com>
2024-05-01 16:15:48 +0100

    feat: waits for document drive server to be loaded before checking default drive is added

--- src/hooks/useLoadDefaultDrive.ts
+++ src/hooks/useLoadDefaultDrive.ts
@@ -4,14 +4,14 @@ import { useFeatureFlag } from './useFeatureFlags';
 
 export const useLoadDefaultDrive = () => {
     const loading = useRef(false);
-    const { addRemoteDrive, documentDrives } = useDocumentDriveServer();
+    const { addRemoteDrive, documentDrives, documentDrivesStatus } = useDocumentDriveServer();
     const {
         setConfig,
         config: { defaultDrive },
     } = useFeatureFlag();
 
     useEffect(() => {
-        if (defaultDrive && !defaultDrive.loaded && !loading.current) {
+        if (defaultDrive && documentDrivesStatus === "LOADED" && !defaultDrive.loaded && !loading.current) {
             const isDriveAlreadyAdded = documentDrives.some(drive => {
                 return drive.state.local.triggers.some(
                     trigger => trigger.data?.url === defaultDrive.url,
@@ -56,7 +56,7 @@ export const useLoadDefaultDrive = () => {
                 .catch(console.error)
                 .finally(() => (loading.current = false));
         }
-    }, [documentDrives, defaultDrive]);
+    }, [documentDrives, defaultDrive, documentDrivesStatus]);
 
     return loading.current;
 };
