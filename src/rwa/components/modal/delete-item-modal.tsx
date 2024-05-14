af139397c154977d68ca73536317690426469b53
ryanwolhuter <dev@ryanwolhuter.com>
2024-05-14 15:28:24 +0200

    feat: fix button styles
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/rwa/components/modal/delete-item-modal.tsx
+++ src/rwa/components/modal/delete-item-modal.tsx
@@ -1,21 +1,11 @@
-import { DivProps, Icon, Modal, mergeClassNameProps } from '@/powerhouse';
+import { Icon, Modal } from '@/powerhouse';
 import React, { ComponentPropsWithoutRef, Fragment } from 'react';
-import { Button, ButtonProps } from 'react-aria-components';
-import { twMerge } from 'tailwind-merge';
-
-const buttonStyles =
-    'min-h-[48px] min-w-[142px] text-base font-semibold py-3 px-6 rounded-xl outline-none active:opacity-75 hover:scale-105 transform transition-all';
 
 export type RWADeleteItemModalProps = ComponentPropsWithoutRef<typeof Modal> & {
     itemName: React.ReactNode;
     dependentItemName: React.ReactNode;
     dependentItemList: React.ReactNode[];
     onContinue: () => void;
-    bodyProps?: DivProps;
-    continueButtonProps?: ButtonProps;
-    headerProps?: DivProps;
-    buttonContainerProps?: DivProps;
-    containerProps?: DivProps;
 };
 
 export const RWADeleteItemModal = (props: RWADeleteItemModalProps) => {
@@ -25,41 +15,22 @@ export const RWADeleteItemModal = (props: RWADeleteItemModalProps) => {
         dependentItemList,
         onOpenChange,
         onContinue,
-        overlayProps,
-        contentProps,
-        bodyProps = {},
-        headerProps = {},
-        containerProps = {},
-        continueButtonProps = {},
-        buttonContainerProps = {},
         ...restProps
     } = props;
 
     return (
         <Modal
             overlayProps={{
-                ...overlayProps,
-                className: twMerge('top-10', overlayProps?.className),
+                className: 'top-10',
             }}
             contentProps={{
-                ...contentProps,
-                className: twMerge('rounded-3xl', contentProps?.className),
+                className: 'rounded-3xl',
             }}
             onOpenChange={onOpenChange}
             {...restProps}
         >
-            <div
-                {...mergeClassNameProps(
-                    containerProps,
-                    'w-[400px] p-6 text-slate-300',
-                )}
-            >
-                <div
-                    {...mergeClassNameProps(
-                        headerProps,
-                        'border-b border-slate-50 pb-2 text-2xl font-bold text-gray-800',
-                    )}
-                >
+            <div className="w-[400px] p-6 text-slate-300">
+                <div className="border-b border-slate-50 pb-2 text-2xl font-bold text-gray-800">
                     Cannot delete {itemName}
                 </div>
                 <div className="my-6 flex gap-2 rounded-md bg-orange-100 p-4 text-orange-800">
@@ -72,34 +43,18 @@ export const RWADeleteItemModal = (props: RWADeleteItemModalProps) => {
                         delete those first.
                     </div>
                 </div>
-                <div
-                    {...mergeClassNameProps(
-                        bodyProps,
-                        'my-6 rounded-md bg-slate-50 p-4 text-slate-200',
-                    )}
-                >
+                <div className="my-6 rounded-md bg-slate-50 p-4 text-slate-200">
                     {dependentItemList.map((item, index) => (
                         <Fragment key={index}>{item}</Fragment>
                     ))}
                 </div>
-                <div
-                    {...mergeClassNameProps(
-                        buttonContainerProps,
-                        'mt-8 flex justify-between gap-3',
-                    )}
-                >
-                    <Button
-                        onPress={onContinue}
-                        {...mergeClassNameProps(
-                            continueButtonProps,
-                            twMerge(
-                                buttonStyles,
-                                'flex-1 bg-gray-800 text-gray-50',
-                            ),
-                        )}
+                <div className="mt-8 flex justify-between gap-3">
+                    <button
+                        onClick={onContinue}
+                        className="min-h-12 min-w-36 flex-1 rounded-xl bg-gray-800 px-6 py-3 text-base font-semibold text-gray-50 outline-none transition-all hover:scale-105 active:opacity-75"
                     >
                         Back
-                    </Button>
+                    </button>
                 </div>
             </div>
         </Modal>
