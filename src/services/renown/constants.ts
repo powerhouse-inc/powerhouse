b2ddc9dda69791a75047afd4ad8436ad6e3cc327
acaldas <acaldas.dev@gmail.com>
2024-05-22 15:58:40 +0200

    feat: updated renown url

--- src/services/renown/constants.ts
+++ src/services/renown/constants.ts
@@ -1,5 +1,5 @@
 export const RENOWN_URL =
-    (import.meta.env.VITE_RENOWN_URL as string) || 'https://renown.vercel.app';
+    (import.meta.env.VITE_RENOWN_URL as string) || 'https://renown.id';
 export const RENOWN_NETWORK_ID =
     (import.meta.env.VITE_RENOWN_NETWORK_ID as string) || 'eip155';
 export const RENOWN_CHAIN_ID =
