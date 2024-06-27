edd4152cb287d2ed3f0d58e52fa274bb9b792431
ryanwolhuter <dev@ryanwolhuter.com>
2024-06-27 14:41:57 +0200

    feat: fix name prop
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/rwa/components/inputs/text-input.tsx
+++ src/rwa/components/inputs/text-input.tsx
@@ -15,7 +15,6 @@ export const _RWATableTextInput = forwardRef(function RWATableTextInput(
 ) {
     const invalid = props['aria-invalid'] === 'true';
     const {
-        name,
         errorMessage,
         labelClassName,
         inputClassName,
