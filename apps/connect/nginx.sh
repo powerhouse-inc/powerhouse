172ef3fed564b9e04d5d33031f69d0562f0d7574
acaldas <acaldas.dev@gmail.com>
2024-09-03 20:31:35 +0100

    feat: entrypoint script cleanup

--- apps/connect/nginx.sh
+++ apps/connect/nginx.sh
@@ -5,8 +5,6 @@
 
 # Prepare nginx config
 envsubst '$PORT,$BASE_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
-nginx -t
 
 # Start Nginx
 nginx -g 'daemon off;'
