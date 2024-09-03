6123f034bd667aac34012df1627dc255922703bc
acaldas <acaldas.dev@gmail.com>
2024-09-03 20:22:44 +0100

    feat: use script as entrypoint

--- apps/connect/nginx.sh
+++ apps/connect/nginx.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Execute vite-envs.sh
+/usr/share/nginx/html/vite-envs.sh
+
+# Prepare nginx config
 envsubst '$PORT,$BASE_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
 cat /etc/nginx/conf.d/default.conf
-
 nginx -t
+
+# Start Nginx
+nginx -g 'daemon off;'
