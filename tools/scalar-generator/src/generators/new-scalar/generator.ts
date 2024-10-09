352ca938221739a4c8d69b502d14ae13f8cf1690
Guillermo Puente <guillermo.ps09@gmail.com>
2024-10-09 11:34:36 -0300

    fix(scalar): rebase with main and fix conflicts

--- tools/scalar-generator/src/generators/new-scalar/generator.ts
+++ tools/scalar-generator/src/generators/new-scalar/generator.ts
@@ -1,57 +1,57 @@
-import { formatFiles, generateFiles, Tree } from '@nx/devkit';
-import * as path from 'path';
-import { NewScalarGeneratorSchema } from './schema';
+import { formatFiles, generateFiles, Tree } from "@nx/devkit";
+import * as path from "path";
+import { NewScalarGeneratorSchema } from "./schema";
 
 const namespaceImport =
-    '// namespace imports -- DO NOT REMOVE OR EDIT THIS COMMENT';
-const exportObject = '// export object -- DO NOT REMOVE OR EDIT THIS COMMENT';
+  "// namespace imports -- DO NOT REMOVE OR EDIT THIS COMMENT";
+const exportObject = "// export object -- DO NOT REMOVE OR EDIT THIS COMMENT";
 const exportResolvers =
-    '// export resolvers -- DO NOT REMOVE OR EDIT THIS COMMENT';
+  "// export resolvers -- DO NOT REMOVE OR EDIT THIS COMMENT";
 const exportTypedefs =
-    '// export typedefs -- DO NOT REMOVE OR EDIT THIS COMMENT';
+  "// export typedefs -- DO NOT REMOVE OR EDIT THIS COMMENT";
 
 const exportScalarType =
-    '// export types -- DO NOT REMOVE OR EDIT THIS COMMENT';
+  "// export types -- DO NOT REMOVE OR EDIT THIS COMMENT";
 
 export async function newScalarGenerator(
-    tree: Tree,
-    options: NewScalarGeneratorSchema,
+  tree: Tree,
+  options: NewScalarGeneratorSchema,
 ) {
-    const projectRoot = `packages/scalars`;
+  const projectRoot = `packages/scalars`;
 
-    generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  generateFiles(tree, path.join(__dirname, "files"), projectRoot, options);
 
-    const filePath = `packages/scalars/src/scalars/index.ts`;
-    const contents = tree.read(filePath).toString();
+  const filePath = `packages/scalars/src/scalars/index.ts`;
+  const contents = tree.read(filePath)?.toString() || "";
 
-    let newContents = contents.replace(
-        namespaceImport,
-        `${namespaceImport}\nimport * as ${options.name} from './${options.name}';`,
-    );
+  let newContents = contents.replace(
+    namespaceImport,
+    `${namespaceImport}\nimport * as ${options.name} from './${options.name}';`,
+  );
 
-    newContents = newContents.replace(
-        exportObject,
-        `${exportObject}\n    ${options.name},`,
-    );
+  newContents = newContents.replace(
+    exportObject,
+    `${exportObject}\n    ${options.name},`,
+  );
 
-    newContents = newContents.replace(
-        exportResolvers,
-        `${exportResolvers}\n    ${options.name}: ${options.name}.scalar,`,
-    );
+  newContents = newContents.replace(
+    exportResolvers,
+    `${exportResolvers}\n    ${options.name}: ${options.name}.scalar,`,
+  );
 
-    newContents = newContents.replace(
-        exportTypedefs,
-        `${exportTypedefs}\n    ${options.name}.typedef,`,
-    );
+  newContents = newContents.replace(
+    exportTypedefs,
+    `${exportTypedefs}\n    ${options.name}.typedef,`,
+  );
 
-    newContents = newContents.replace(
-        exportScalarType,
-        `${exportScalarType}\nexport type { ScalarType as ${options.name}ScalarType } from './${options.name}';`,
-    );
+  newContents = newContents.replace(
+    exportScalarType,
+    `${exportScalarType}\nexport type { ScalarType as ${options.name}ScalarType } from './${options.name}';`,
+  );
 
-    tree.write(filePath, newContents);
+  tree.write(filePath, newContents);
 
-    await formatFiles(tree);
+  await formatFiles(tree);
 }
 
 export default newScalarGenerator;
--- tools/scalar-generator/src/generators/new-scalar/schema.d.ts
+++ tools/scalar-generator/src/generators/new-scalar/schema.d.ts
@@ -1,3 +1,3 @@
 export interface NewScalarGeneratorSchema {
-    name: string;
+  name: string;
 }
