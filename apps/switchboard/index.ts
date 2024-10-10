97dde9c8657f2cb2d0d03f7a01a129ade5b9a646
Frank <frank.pfeift@protonmail.com>
2024-10-10 12:01:58 +0200

    chore(switchboard): improved startup

--- apps/switchboard/index.ts
+++ apps/switchboard/index.ts
@@ -6,9 +6,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { DocumentDriveServer } from 'document-drive';
 import * as DocumentModelsLibs from 'document-model-libs/document-models';
-import { DocumentModel } from "document-model/document";
+import { DocumentModel } from 'document-model/document';
 import { module as DocumentModelLib } from 'document-model/document-model';
-import { drizzle } from "drizzle-orm/connect";
+import { drizzle } from 'drizzle-orm/connect';
 import express from 'express';
 import http from 'http';
 import { getSchema as getAuthSchema } from './subgraphs/auth/subgraph';
@@ -17,32 +17,34 @@ import { getSchema as getRwaReadModelSchema } from './subgraphs/rwa-read-model/s
 import { getSchema as getSystemSchema } from './subgraphs/system/subgraph';
 import { InternalListenerManager } from './utils/internal-listener-manager';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import { DrizzleD1Database } from 'drizzle-orm/d1';
+
 dotenv.config();
 
 export const SUBGRAPH_REGISTRY = [
     {
         name: 'system',
-        getSchema: getSystemSchema
+        getSchema: getSystemSchema,
     },
     {
         name: 'auth',
-        getSchema: getAuthSchema
+        getSchema: getAuthSchema,
     },
     {
         name: ':drive/rwa-read-model',
-        getSchema: getRwaReadModelSchema
+        getSchema: getRwaReadModelSchema,
     },
     {
         name: ':drive',
-        getSchema: getDriveSchema
-    }
+        getSchema: getDriveSchema,
+    },
 ];
 
 // start document drive server with all available document models
 const driveServer = new DocumentDriveServer([
     DocumentModelLib,
-    ...Object.values(DocumentModelsLibs)
+    ...Object.values(DocumentModelsLibs),
 ] as DocumentModel[]);
 
 const getLocalSubgraphConfig = (subgraphName: string) =>
@@ -54,7 +56,10 @@ const app = express();
 const serverPort = process.env.PORT ? Number(process.env.PORT) : 4001;
 const httpServer = http.createServer(app);
 
-const db = process.env.DATABASE_URL !== "" ? drizzle("node-postgres", process.env.DATABASE_URL as string) : drizzle('pglite');
+// @TODO: app.use frontend on /
+
+// @TODO: make this more specific
+let db: Omit<DrizzleD1Database, 'batch' | 'values' | 'run' | 'all' | 'get'>;
 
 export const updateRouter = async () => {
     const newRouter = express.Router();
@@ -70,53 +75,75 @@ export const updateRouter = async () => {
         const server = new ApolloServer({
             schema,
             introspection: true,
-            plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginInlineTraceDisabled()]
+            plugins: [
+                ApolloServerPluginDrainHttpServer({ httpServer }),
+                ApolloServerPluginInlineTraceDisabled(),
+            ],
         });
 
         // start apollo server
         await server.start();
 
         // setup path
-        const path = `/${subgraphConfig.name}/graphql`;
+        const path = `/${subgraphConfig.name}`;
         newRouter.use(
             path,
             cors(),
             bodyParser.json(),
             expressMiddleware(server, {
-                context: async ({ req }) => ({
-                    user: req.headers.authorization?.replace('Bearer ', ''),
-                    driveId: req.params.drive ?? undefined,
-                    driveServer,
-                    db
-                })
-            })
+                context: ({ req }) =>
+                    Promise.resolve({
+                        user: req.headers.authorization?.replace('Bearer ', ''),
+                        driveId: req.params.drive ?? undefined,
+                        driveServer,
+                        db,
+                    }),
+            }),
         );
 
-        console.log(`Setting up [${subgraphConfig.name}] subgraph at http://localhost:${serverPort}${path}`);
+        console.log(
+            `Setting up [${subgraphConfig.name}] subgraph at http://localhost:${serverPort}${path}`,
+        );
     }
     router = newRouter;
-    console.log('All subgraphs started.')
+    console.log('All subgraphs started.');
 };
 
-(async () => {
-    // init drive server
-    await driveServer.initialize();
+const main = async () => {
+    try {
+        // init db
+        if (process.env.DATABASE_URL && process.env.DATABASE_URL !== '') {
+            // @ts-expect-error: linter doesnt see that drizzle returns a promise
+            db = await drizzle('node-postgres', process.env.DATABASE_URL);
+        } else {
+            // @ts-expect-error: linter doesnt see that drizzle returns a promise
+            db = await drizzle('pglite');
+        }
 
-    // init listener manager
-    const listenerManager = new InternalListenerManager(driveServer);
-    await listenerManager.init();
+        // init drive server
+        await driveServer.initialize();
 
-    // init router
-    await updateRouter();
-    app.use(router);
+        // init listener manager
+        const listenerManager = new InternalListenerManager(driveServer);
+        await listenerManager.init();
 
-    // start http server
-    httpServer.listen({ port: serverPort }, () => {
-        console.log(`Subgraph server listening on port ${serverPort}`);
-    });
-
-    // update router with new document models on document models change
-    driveServer.on("documentModels", async (documentModels) => {
+        // init router
         await updateRouter();
-    });
-})();
+
+        app.use('/graphql', router);
+
+        // start http server
+        httpServer.listen({ port: serverPort }, () => {
+            console.log(`Subgraph server listening on port ${serverPort}`);
+        });
+
+        // update router with new document models on document models change
+        driveServer.on('documentModels', async documentModels => {
+            await updateRouter();
+        });
+    } catch (e) {
+        console.error('App crashed', e);
+    }
+};
+
+main().catch(console.error);
