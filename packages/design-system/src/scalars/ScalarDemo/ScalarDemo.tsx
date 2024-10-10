65a7e8029b1a8e21dbff90cade9b996c70fba618
Guillermo Puente <guillermo.ps09@gmail.com>
2024-10-10 15:41:15 -0300

    fix(design-system): update demo scalar

--- packages/design-system/src/scalars/ScalarDemo/ScalarDemo.tsx
+++ packages/design-system/src/scalars/ScalarDemo/ScalarDemo.tsx
@@ -3,5 +3,5 @@ export interface ScalarDemoProps {
 }
 
 export const ScalarDemo: React.FC<ScalarDemoProps> = ({ name }) => (
-  <div className="rounded-md bg-gray-100 p-3">Scalar Demo: {name}</div>
+  <div className="rounded-md bg-yellow-200 p-3">Scalar Demo: {name}</div>
 );
