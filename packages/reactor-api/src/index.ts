7b30ed2fd25111fc7b900c1f3ba1d468d076c50f
acaldas <acaldas.dev@gmail.com>
2024-10-11 06:52:23 +0100

    feat(reactor-api): init project

---
+++ packages/reactor-api/src/index.ts
@@ -0,0 +1,19 @@
+import { IDocumentDriveServer } from "document-drive";
+import express, { Express } from "express";
+import { router } from "./router";
+
+type Options = {
+  express?: Express;
+  port?: number;
+};
+
+const DEFAULT_PORT = 4000;
+
+export function startAPI(reactor: IDocumentDriveServer, options: Options) {
+  const port = options.port ?? DEFAULT_PORT;
+  const app = options.express ?? express();
+
+  app.use(router);
+
+  app.listen(port);
+}
