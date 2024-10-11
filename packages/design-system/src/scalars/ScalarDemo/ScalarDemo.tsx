68a19c91ce610880e73effaca30db6d1d9fe707e
Guillermo Puente <guillermo.ps09@gmail.com>
2024-10-11 13:11:07 -0300

    chore: added scalars package release

--- packages/design-system/src/scalars/ScalarDemo/ScalarDemo.tsx
+++ packages/design-system/src/scalars/ScalarDemo/ScalarDemo.tsx
@@ -1,7 +1,14 @@
+import { schema } from "@powerhousedao/scalars/EmailAddress";
+
 export interface ScalarDemoProps {
   name: string;
 }
 
-export const ScalarDemo: React.FC<ScalarDemoProps> = ({ name }) => (
-  <div className="rounded-md bg-yellow-200 p-3">Scalar Demo: {name}</div>
-);
+export const ScalarDemo: React.FC<ScalarDemoProps> = ({ name }) => {
+  const result = schema.safeParse(name);
+  console.log(result);
+
+  return (
+    <div className="rounded-md bg-yellow-200 p-3">Scalar Demo: {name}</div>
+  );
+};
