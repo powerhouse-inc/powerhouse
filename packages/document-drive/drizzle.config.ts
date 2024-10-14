40c9f89137dc46006723f3c3aa2e1cd92b02c516
Frank <frank.pfeift@protonmail.com>
2024-10-14 09:39:51 +0200

    chore(document-drive): updated drizzle config

--- packages/document-drive/drizzle.config.ts
+++ packages/document-drive/drizzle.config.ts
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
-dotenv.config();
 
-console.log(process.env.DATABASE_URL);
 export default process.env.DATABASE_URL !== "" &&
 process.env.DATABASE_URL !== undefined
   ? defineConfig({
       dialect: "postgresql",
-      schema: "./subgraphs/*/schema.ts",
+      schema: "./src/storage/drizzle/schema.ts",
       out: "./drizzle",
       dbCredentials: {
         url: process.env.DATABASE_URL,
@@ -16,7 +13,7 @@ process.env.DATABASE_URL !== undefined
   : defineConfig({
       driver: "pglite",
       dialect: "postgresql",
-      schema: "./subgraphs/*/schema.ts",
+      schema: "./src/storage/drizzle/schema.ts",
       dbCredentials: {
         url: "file:./dev.db",
       },
