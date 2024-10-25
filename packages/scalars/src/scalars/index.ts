ab17138f94dfe053d7e8b33fafa69c30949a7915
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2024-10-25 12:05:34 -0300

    feat(scalars): added amount percentage scalar (#433)

--- packages/scalars/src/scalars/index.ts
+++ packages/scalars/src/scalars/index.ts
@@ -1,20 +1,25 @@
 // namespace imports -- DO NOT REMOVE OR EDIT THIS COMMENT
+import * as AmountPercentage from './AmountPercentage';
 import * as EmailAddress from './EmailAddress';
 
 // export types -- DO NOT REMOVE OR EDIT THIS COMMENT
+export type { ScalarType as AmountPercentageScalarType } from './AmountPercentage';
 export type { ScalarType as EmailAddressScalarType } from './EmailAddress';
 
 export {
     // export object -- DO NOT REMOVE OR EDIT THIS COMMENT
+    AmountPercentage,
     EmailAddress,
 };
 
 export const resolvers = {
     // export resolvers -- DO NOT REMOVE OR EDIT THIS COMMENT
+    AmountPercentage: AmountPercentage.scalar,
     EmailAddress: EmailAddress.scalar,
 };
 
 export const typeDefs = [
     // export typedefs -- DO NOT REMOVE OR EDIT THIS COMMENT
+    AmountPercentage.typedef,
     EmailAddress.typedef,
 ];
