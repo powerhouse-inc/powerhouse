ff1e60b351e91bb052a7ff1a27ef8bf5239ee339
Frank <frank.pfeift@protonmail.com>
2024-10-30 17:41:31 +0100

    fix(codegen): json document model import

--- packages/codegen/src/codegen/utils.ts
+++ packages/codegen/src/codegen/utils.ts
@@ -17,9 +17,12 @@ export async function loadDocumentModel(
       documentModel = file.state.global;
     } else if (path.endsWith(".json")) {
       const data = fs.readFileSync(path, "utf-8");
-      const document = (JSON.parse(data) as DocumentModel).state
-        .global as DocumentModelState;
-      documentModel = document;
+      const parsedData = JSON.parse(data) as DocumentModel | DocumentModelState;
+      if ("state" in parsedData) {
+        documentModel = parsedData.state.global as DocumentModelState;
+      } else {
+        documentModel = parsedData;
+      }
     } else {
       throw new Error("File type not supported. Must be zip or json.");
     }
