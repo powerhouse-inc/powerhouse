e367c90355446b89ae482e56cf33f07cff554d26
Guillermo Puente <guillermo.ps09@gmail.com>
2024-11-06 10:15:21 +0700

    feat(design-system): test vercel deployment

--- packages/design-system/src/scalars/components/scalar-demo/scalar-demo.test.tsx
+++ packages/design-system/src/scalars/components/scalar-demo/scalar-demo.test.tsx
@@ -2,6 +2,7 @@ import { render } from "@testing-library/react";
 
 import { ScalarDemo } from "./scalar-demo";
 
+// test2
 describe("ScalarDemo Component", () => {
   it("should match snapshot", () => {
     const { asFragment } = render(<ScalarDemo name="test" />);
