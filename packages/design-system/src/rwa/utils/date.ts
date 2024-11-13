d51f80999a618684fb5e7f0790c0793bd0948ca4
ryanwolhuter <dev@ryanwolhuter.com>
2024-11-13 13:05:04 +0200

    feat(design-system): use intl format for datetime inputs
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/design-system/src/rwa/utils/date.ts
+++ packages/design-system/src/rwa/utils/date.ts
@@ -1,20 +1,25 @@
 import { formatInTimeZone } from "date-fns-tz/formatInTimeZone";
-import { format } from "date-fns/format";
 
-/**
- * The html datetime local input requires this specific format
- */
-export function convertToDateTimeLocalFormat(date: Date | string = new Date()) {
-  return format(date, "yyyy-MM-dd'T'HH:mm");
-}
 export function formatDateForDisplay(date: Date | string, displayTime = true) {
   const formatString = displayTime ? "yyyy/MM/dd, HH:mm:ss zzz" : "yyyy/MM/dd";
-
-  return formatInTimeZone(date, "UTC", formatString);
+  return formatInTimeZone(date, getTimeZone(), formatString);
 }
 
 export function isISODate(str: string) {
   if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
   const d = new Date(str);
   return d instanceof Date && !isNaN(d.getTime()) && d.toISOString() === str;
+}
+
+export function getTimeZone() {
+  return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+
+export function isoDateStringToDateInput(
+  isoDateString: string,
+  withTime = false,
+) {
+  const format = withTime ? "yyyy-MM-dd'T'HH:mm:ss.SSS" : "yyyy-MM-dd";
+  const timeZone = getTimeZone();
+  return formatInTimeZone(isoDateString, timeZone, format);
 }
