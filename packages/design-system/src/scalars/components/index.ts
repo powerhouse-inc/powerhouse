a1ec3be6904e9716d5ba94f678acb5c1456b3d41
Yasiel Cabrera <yasiel9506@gmail.com>
2024-11-14 11:56:38 -0500

    feat(scalars): expose framgets

--- packages/design-system/src/scalars/components/index.ts
+++ packages/design-system/src/scalars/components/index.ts
@@ -3,3 +3,5 @@ export * from "./string-field";
 export * from "./boolean-field";
 export * from "./number-field";
 export * from "./url-field";
+export * from "./fragments";
+export * from "./types";
