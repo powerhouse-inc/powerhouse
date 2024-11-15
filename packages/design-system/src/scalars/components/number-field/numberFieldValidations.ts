2b1e030ecfd299e1f62c7ca7bad1f3e734cdb48f
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-11-15 10:24:21 -0300

    fix(scalars): update the validations number and export the select-field

--- packages/design-system/src/scalars/components/number-field/numberFieldValidations.ts
+++ packages/design-system/src/scalars/components/number-field/numberFieldValidations.ts
@@ -1,54 +1,57 @@
 import { NumberFieldProps } from "./number-field";
 
 export const validatePositive =
-  (props: NumberFieldProps) => (value: string) => {
+  (props: NumberFieldProps) => (value: unknown) => {
+    const stringValue = String(value);
     return props.allowNegative
       ? true
-      : Number(value) > 0
+      : Number(stringValue) > 0
         ? true
-        : "Value must be a poasdasdassitive value";
+        : "Value must be a positive value";
   };
 
 export const validateIsBigInt =
-  (props: NumberFieldProps) => (value: string) => {
-    const isLargeNumber = Math.abs(Number(value)) > Number.MAX_SAFE_INTEGER;
+  (props: NumberFieldProps) => (value: unknown) => {
+    const stringValue = String(value);
+    const isLargeNumber =
+      Math.abs(Number(stringValue)) > Number.MAX_SAFE_INTEGER;
     return isLargeNumber && !props.isBigInt
       ? "Value is too large for standard integer"
       : true;
   };
 
 export const validatePrecision =
-  (props: NumberFieldProps) => (value: string) => {
-    // Si precision no está definida, solo permitimos enteros
+  (props: NumberFieldProps) => (value: unknown) => {
+    const stringValue = String(value);
     if (props.precision === undefined) {
-      return !value.includes(".") ? true : "Value must be an integer";
+      return !stringValue.includes(".") ? true : "Value must be an integer";
     }
 
-    // Comprobamos la parte decimal del valor si se especifica precision
-    const decimalPart = value.toString().split(".")[1];
+    const decimalPart = stringValue.split(".")[1];
     if (props.precision === 0) {
       return !decimalPart ? true : "Value must be an integer";
     }
 
-    // Validamos la cantidad de decimales cuando precision está definida
     return decimalPart && decimalPart.length <= props.precision
       ? true
       : `Value must have ${props.precision} decimal places or fewer`;
   };
 
 export const validateTrailingZeros =
-  (props: NumberFieldProps) => (value: string) => {
+  (props: NumberFieldProps) => (value: unknown) => {
+    const stringValue = String(value);
     if (!props.trailingZeros) return true;
     const hasTrailingZeros =
-      value.toString().split(".")[1]?.length === props.precision;
+      stringValue.split(".")[1]?.length === props.precision;
     return hasTrailingZeros
       ? true
       : `Value must have exactly ${props.precision} decimal places`;
   };
 
 export const validateDecimalRequired =
-  (props: NumberFieldProps) => (value: string) => {
-    return props.decimalRequired && !value.includes(".")
+  (props: NumberFieldProps) => (value: unknown) => {
+    const stringValue = String(value);
+    return props.decimalRequired && !stringValue.includes(".")
       ? "Value must include a decimal point"
       : true;
   };
