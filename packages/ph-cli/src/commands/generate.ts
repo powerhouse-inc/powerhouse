c43fcd53beb3ae280a7d11721231636203514af8
frankp.eth <froid1911@users.noreply.github.com>
2024-11-27 18:27:45 +0100

    feat(reactor-local): added automatic loading and activation of proces… (#574)
    * feat(reactor-local): added automatic loading and activation of processors
    
    * chore(ph-cli): replaced ts-node with tsx
    
    * chore(ph-cli): added deps for analytics processor
    
    * feat(ph-cli): added processor generate cmd
    
    * chore: updated lockfile
    
    * fix(ph-cli): generate processor command
    
    * chore: lockfile
    
    * fix(ph-cli): linting issues

--- packages/ph-cli/src/commands/generate.ts
+++ packages/ph-cli/src/commands/generate.ts
@@ -5,6 +5,7 @@ import {
   generateEditor,
   generateFromFile,
   promptDirectories,
+  generateProcessor,
 } from "@powerhousedao/codegen";
 import { CommandActionType } from "../types.js";
 
@@ -14,10 +15,12 @@ export const generate: CommandActionType<
     {
       interactive?: boolean;
       editors?: string;
+      processors?: string;
       documentModels?: string;
       skipFormat?: boolean;
       watch?: boolean;
       editor?: string;
+      processor?: string;
       documentTypes?: string;
     },
   ]
@@ -28,6 +31,7 @@ export const generate: CommandActionType<
     ...baseConfig,
     ...{
       ...(options.editors && { editorsDir: options.editors }),
+      ...(options.processors && { processorsDir: options.processors }),
       ...(options.documentModels && {
         documentModelsDir: options.documentModels,
       }),
@@ -41,6 +45,8 @@ export const generate: CommandActionType<
     editor: !!options.editor,
     editorName: options.editor,
     documentTypes: options.documentTypes,
+    processor: !!options.processor,
+    processorName: options.processor,
   };
 
   if (config.interactive) {
@@ -61,6 +67,15 @@ export const generate: CommandActionType<
     return;
   }
 
+  if (command.processor && options.processor) {
+    await generateProcessor(
+      options.processor,
+      options.documentTypes?.split(",") ?? [],
+      config,
+    );
+    return;
+  }
+
   if (filePath) {
     await generateFromFile(filePath, config);
     return;
@@ -77,6 +92,8 @@ export function generateCommand(program: Command) {
     .option("-i, --interactive", "Run the command in interactive mode")
     .option("--editors <type>", "Path to the editors directory")
     .option("-e, --editor <type>", "Editor Name")
+    .option("--processors <type>", "Path to the processors directory")
+    .option("-p, --processor <type>", "Processor Name")
     .option("--document-models <type>", "Path to the document models directory")
     .option("--document-types <type>", "Supported document types by the editor")
     .option("-sf, --skip-format", "Skip formatting the generated code")
