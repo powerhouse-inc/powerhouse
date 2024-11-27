e30fb51c33d4801d28dda09550e588dcb778b6fc
ryanwolhuter <dev@ryanwolhuter.com>
2024-11-27 15:50:43 +0200

    feat(document-model-libs): simplify export
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/document-model-libs/editors/document-model-2/components/index.ts
+++ packages/document-model-libs/editors/document-model-2/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./form";
 export * from "./label";
-export * from "./code-editors/graphql-editor";
 export * from "./module-form";
 export * from "./input";
-export * from "./code-editors/json-editor";
+export * from "./code-editors";
