569659b1ea188a39e6805b4a4d698ff69fe9aef1
Yasiel Cabrera <yasiel9506@gmail.com>
2024-11-30 14:25:43 -0500

    fix(scalars): tests

--- packages/design-system/src/scalars/components/fragments/select-field/select-field.tsx
+++ packages/design-system/src/scalars/components/fragments/select-field/select-field.tsx
@@ -124,15 +124,15 @@ export const SelectFieldRaw = React.forwardRef<
               aria-expanded={isPopoverOpen}
               className={cn(
                 "flex h-9 w-full items-center justify-between px-3 py-2",
-                "dark:border-charcoal-700 dark:bg-charcoal-900 rounded-md border border-gray-300 bg-white",
+                "rounded-md border border-gray-300 bg-white dark:border-charcoal-700 dark:bg-charcoal-900",
                 "hover:border-gray-300 hover:bg-gray-100",
                 "dark:hover:border-charcoal-700 dark:hover:bg-charcoal-800",
-                "dark:focus:ring-charcoal-300 focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-0",
-                "dark:focus-visible:ring-charcoal-300 focus-visible:ring-1 focus-visible:ring-gray-900 focus-visible:ring-offset-0",
+                "focus:outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-0 dark:focus:ring-charcoal-300",
+                "focus-visible:ring-1 focus-visible:ring-gray-900 focus-visible:ring-offset-0 dark:focus-visible:ring-charcoal-300",
                 disabled && [
                   "opacity-50",
                   "cursor-not-allowed",
-                  "dark:hover:border-charcoal-700 dark:hover:bg-charcoal-900 hover:border-gray-300 hover:bg-white",
+                  "hover:border-gray-300 hover:bg-white dark:hover:border-charcoal-700 dark:hover:bg-charcoal-900",
                 ],
                 className,
               )}
