d95026e9590da3e05ebbe2be5d3d890be16fe95a
Frank <frank.pfeift@protonmail.com>
2024-12-02 12:26:36 +0100

    refactor(switchboard): use new reactor api

--- apps/switchboard/src/index.ts
+++ apps/switchboard/src/index.ts
@@ -1,23 +1,15 @@
+import * as searchListener from "@powerhousedao/general-document-indexer";
+import { ReactorRouterManager } from "@powerhousedao/reactor-api";
 import { DocumentDriveServer } from "document-drive";
 import * as DocumentModelsLibs from "document-model-libs/document-models";
 import { DocumentModel } from "document-model/document";
 import { module as DocumentModelLib } from "document-model/document-model";
 import dotenv from "dotenv";
-import { drizzle } from "drizzle-orm/connect";
 import express from "express";
 import http from "http";
-import {
-  addSubgraph,
-  createSchema,
-  initReactorRouter,
-  registerInternalListener,
-  setAdditionalContextFields,
-} from "@powerhousedao/reactor-api";
-import * as searchListener from "@powerhousedao/general-document-indexer";
-import { getSchema as getAuthSchema } from "./subgraphs/auth/subgraph";
 import path from "path";
-import { GraphQLResolverMap } from "@apollo/subgraph/dist/schema-helper";
-
+import { Pool } from "pg";
+import * as authListener from "./subgraphs/auth";
 dotenv.config();
 
 // start document drive server with all available document models
@@ -33,54 +25,43 @@ const httpServer = http.createServer(app);
 let db: any;
 const main = async () => {
   try {
-    // init db
-    if (process.env.DATABASE_URL && process.env.DATABASE_URL !== "") {
-      db = await drizzle("node-postgres", process.env.DATABASE_URL);
-    } else {
-      db = await drizzle("pglite", "./dev.db");
-    }
-
     // init drive server
     await driveServer.initialize();
 
-    // init router
-    await initReactorRouter("/", app, driveServer);
-
-    setAdditionalContextFields({ db });
-
-    // add search subgraph @todo: automatically add all subgraphs
-    await addSubgraph({
-      name: "search/:drive",
-      getSchema: (driveServer) =>
-        createSchema(
-          driveServer,
-          searchListener.resolvers as GraphQLResolverMap,
-          searchListener.typeDefs,
-        ),
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
     });
+    await pool.connect();
+    const reactorRouterManager = new ReactorRouterManager(
+      "/",
+      app,
+      driveServer,
+      pool
+    );
+    // init router
+    await reactorRouterManager.init();
 
-    await registerInternalListener({
+    await reactorRouterManager.registerProcessor({
       ...searchListener,
-      name: "general-document-indexer",
       transmit(strands) {
         return searchListener.transmit(strands, db);
       },
+      name: "search/:drive",
     });
 
-    // add auth subgraph
-    await addSubgraph({
-      name: "auth",
-      getSchema: (driveServer) => getAuthSchema(driveServer),
-    });
+    // @TODO: add auth listener
+    // await reactorRouterManager.addSubgraph({
+    //   ...authListener,
+    // });
 
     // load switchboard-gui
     app.use(
       express.static(
         path.join(
           __dirname,
-          "../node_modules/@powerhousedao/switchboard-gui/dist",
-        ),
-      ),
+          "../node_modules/@powerhousedao/switchboard-gui/dist"
+        )
+      )
     );
 
     // start http server
