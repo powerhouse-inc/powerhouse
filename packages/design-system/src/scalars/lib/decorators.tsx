0c43d32d4361f6aa29372f317e1c805b8ee87465
Yasiel Cabrera <yasiel9506@gmail.com>
2024-12-03 15:26:09 -0500

    fix(scalars): reset function on stories

--- packages/design-system/src/scalars/lib/decorators.tsx
+++ packages/design-system/src/scalars/lib/decorators.tsx
@@ -3,15 +3,37 @@ import { Decorator } from "@storybook/react";
 import { Checkbox, Form } from "../components";
 import { Button } from "@/powerhouse";
 import { useState, useId } from "react";
+import { useCallback } from "react";
+import { useRef } from "react";
+import { UseFormReturn } from "react-hook-form";
 
 export const withForm: Decorator = (Story, context) => {
+  const formRef = useRef<UseFormReturn>(null);
   const [showFormButtons, setShowFormButtons] = useState<boolean>(false);
   const checkboxId = useId();
   const { viewMode } = context;
 
-  const onSubmit = (data: any) => {
+  const onSubmit = useCallback((data: any) => {
     alert(JSON.stringify(data, null, 2));
-  };
+  }, []);
+
+  const onReset = useCallback(() => {
+    // reset only works if the form has default values to reset to
+    // so as this is a generic decorator, we need to build the default values
+    // from the control fields. This is not how the reset is going to be used
+    // in the real world, but it's good enough for the storybook
+    const defaultValues = Object.fromEntries(
+      Object.keys(formRef.current?.control._fields ?? {}).map((fieldName) => [
+        fieldName,
+        "",
+      ]),
+    );
+    formRef.current?.reset(defaultValues);
+  }, []);
+
+  const onShowFormButtonsChange = useCallback((checked: boolean) => {
+    setShowFormButtons(checked);
+  }, []);
 
   // override the warnings in the args to avoid breaking the storybook
   // as storybook set by default an object instead of an array
@@ -24,31 +46,27 @@ export const withForm: Decorator = (Story, context) => {
 
   return (
     <div>
-      <Form onSubmit={onSubmit}>
-        {({ reset }) => (
-          <>
-            <Story args={overrideArgs} />
+      <Form ref={formRef} onSubmit={onSubmit}>
+        <Story args={overrideArgs} />
 
-            {showFormButtons ? (
-              <div className="flex gap-2">
-                <Button
-                  className="mt-4 w-full"
-                  color="light"
-                  type="reset"
-                  onClick={reset}
-                >
-                  Reset
-                </Button>
-                <Button className="mt-4 w-full" color="dark" type="submit">
-                  Submit
-                </Button>
-              </div>
-            ) : (
-              // allow to "submit" the form from the story
-              // it is needed as some browsers do not submit on Enter key press when there're several inputs and no submit button
-              <input type="submit" className="hidden" />
-            )}
-          </>
+        {showFormButtons ? (
+          <div className="flex gap-2">
+            <Button
+              className="mt-4 w-full"
+              color="light"
+              type="reset"
+              onClick={onReset}
+            >
+              Reset
+            </Button>
+            <Button className="mt-4 w-full" color="dark" type="submit">
+              Submit
+            </Button>
+          </div>
+        ) : (
+          // allow to "submit" the form from the story
+          // it is needed as some browsers do not submit on Enter key press when there're several inputs and no submit button
+          <input type="submit" className="hidden" />
         )}
       </Form>
 
@@ -58,9 +76,7 @@ export const withForm: Decorator = (Story, context) => {
             <Checkbox
               id={checkboxId}
               checked={showFormButtons}
-              onCheckedChange={(checked) =>
-                setShowFormButtons(checked as boolean)
-              }
+              onCheckedChange={onShowFormButtonsChange}
             />
             <label
               className="cursor-pointer dark:text-gray-400"
