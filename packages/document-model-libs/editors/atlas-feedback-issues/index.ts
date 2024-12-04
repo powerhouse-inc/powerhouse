848f90621fa80d15e9c98f602a265839bec8a4b5
ryanwolhuter <dev@ryanwolhuter.com>
2024-12-04 14:36:20 +0200

    feat(document-model-libs): update index
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/document-model-libs/editors/atlas-feedback-issues/index.ts
+++ packages/document-model-libs/editors/atlas-feedback-issues/index.ts
@@ -1,11 +1,21 @@
 import { ExtendedEditor, EditorContextProps } from "document-model-libs";
 import Editor from "./editor";
+import {
+  AtlasFeedbackIssuesState,
+  AtlasFeedbackIssuesAction,
+  AtlasFeedbackIssuesLocalState,
+} from "../../document-models/atlas-feedback-issues";
+import { lazyWithPreload } from "document-model-libs/utils";
 
-export const module: ExtendedEditor<unknown, Action, unknown, unknown> = {
-  Component: Editor,
-  documentTypes: ["*"],
+export const module: ExtendedEditor<
+  AtlasFeedbackIssuesState,
+  AtlasFeedbackIssuesAction,
+  AtlasFeedbackIssuesLocalState
+> = {
+  Component: lazyWithPreload(() => import("./editor")),
+  documentTypes: ["makerdao/atlas-feedback-issues"],
   config: {
-    id: "editor-id",
+    id: "makerdao/atlas-feedback-issues",
     disableExternalControls: true,
     documentToolbarEnabled: true,
     showSwitchboardLink: true,
