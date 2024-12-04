5684ba996cac3d09bef892325c4593f262f8ab73
Yasiel Cabrera <yasiel9506@gmail.com>
2024-12-04 18:14:16 -0500

    fix(scalars): character counter when maxLenght <= 0

--- packages/design-system/src/scalars/components/fragments/textarea-field/textarea-field.tsx
+++ packages/design-system/src/scalars/components/fragments/textarea-field/textarea-field.tsx
@@ -162,7 +162,7 @@ const TextareaFieldRaw = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
               {...props}
             />
           </ValueTransformer>
-          {maxLength && (
+          {typeof maxLength === "number" && maxLength > 0 && (
             <div className="mt-0.5 flex justify-end">
               <CharacterCounter maxLength={maxLength} value={value ?? ""} />
             </div>
