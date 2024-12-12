ae309ea87c7e81260e4ac154798a54af285c1530
Frank <frank.pfeift@protonmail.com>
2024-12-12 11:38:36 +0100

    feat(reactor-api): added operational processor

---
+++ packages/reactor-api/src/processors/operational-processor.ts
@@ -0,0 +1,22 @@
+import { IAnalyticsStore } from "@powerhousedao/analytics-engine-core";
+import { Document, OperationScope } from "document-model/document";
+import { Processor } from "./processor";
+import { ProcessorOptions, ProcessorSetupArgs } from "src/types";
+import { Knex } from "knex";
+
+export abstract class OperationalProcessor<
+  D extends Document = Document,
+  S extends OperationScope = OperationScope,
+> extends Processor<D, S> {
+  protected operationalStore: Knex;
+
+  constructor(args: ProcessorSetupArgs, options?: ProcessorOptions) {
+    super(args, options);
+    this.operationalStore = args.dataSources.operationalStore;
+  }
+
+  onSetup(args: ProcessorSetupArgs) {
+    super.onSetup(args);
+    this.operationalStore = args.dataSources.operationalStore;
+  }
+}
