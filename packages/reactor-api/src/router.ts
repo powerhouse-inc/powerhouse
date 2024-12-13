5cbe5356a0d7baaee8618a1ff7fd79515a8c1336
Frank <frank.pfeift@protonmail.com>
2024-12-13 12:35:24 +0100

    refactor(reactor-api): subgraph router management

--- packages/reactor-api/src/router.ts
+++ packages/reactor-api/src/router.ts
@@ -9,29 +9,11 @@ import express, { IRouter, Router } from "express";
 import { analyticsSubgraph, driveSubgraph, systemSubgraph } from "./subgraphs";
 import { Context, Subgraph } from "./types";
 import { createSchema } from "./utils/create-schema";
+import path from "node:path";
 
 export class ReactorRouterManager {
   private reactorRouter: IRouter = Router();
   private contextFields: Record<string, any> = {};
-
-  // @todo: need to persist somewhere
-  private subgraphs: Subgraph[] = [
-    {
-      name: "system",
-      resolvers: systemSubgraph.resolvers,
-      typeDefs: systemSubgraph.typeDefs,
-    },
-    {
-      name: "d/:drive",
-      resolvers: driveSubgraph.resolvers,
-      typeDefs: driveSubgraph.typeDefs,
-    },
-    {
-      name: "analytics",
-      resolvers: analyticsSubgraph.resolvers as GraphQLResolverMap<Context>,
-      typeDefs: analyticsSubgraph.typeDefs,
-    },
-  ];
 
   constructor(
     private readonly path: string,
@@ -40,6 +22,8 @@ export class ReactorRouterManager {
   ) {}
 
   async init() {
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
     const models = this.reactor.getDocumentModels();
     const driveModel = models.find(
       (it) => it.documentModel.name === "DocumentDrive",
@@ -48,71 +32,73 @@ export class ReactorRouterManager {
       throw new Error("DocumentDrive model required");
     }
 
-    this.reactor.on("documentModels", () => {
-      this.updateRouter().catch((error: unknown) => console.error(error));
-    });
-
+    // default middleware for dynamic subgraphs
     this.app.use(this.path, (req, res, next) =>
       this.reactorRouter(req, res, next),
     );
 
-    await this.updateRouter();
+    // system Middleware
+    const systemMiddleware = await this.#createSubgraphMiddleware({
+      name: "system",
+      resolvers: systemSubgraph.resolvers,
+      typeDefs: systemSubgraph.typeDefs,
+    });
+    // @ts-expect-error
+    this.app.use(path.join(this.path, "system"), systemMiddleware);
+
+    // drive middleware
+    const driveMiddleware = await this.#createSubgraphMiddleware({
+      name: "d/:drive",
+      resolvers: driveSubgraph.resolvers,
+      typeDefs: driveSubgraph.typeDefs,
+    });
+    // @ts-expect-error
+    this.app.use(path.join(this.path, "d/:drive"), driveMiddleware);
+
+    // analytics middleware
+    const analyticsMiddleware = await this.#createSubgraphMiddleware({
+      name: "analytics",
+      resolvers: analyticsSubgraph.resolvers as GraphQLResolverMap<Context>,
+      typeDefs: analyticsSubgraph.typeDefs,
+    });
+    // @ts-expect-error
+    this.app.use(path.join(this.path, "analytics"), analyticsMiddleware);
   }
 
-  async updateRouter() {
-    const newRouter = Router();
-    newRouter.use(cors());
-    newRouter.use(bodyParser.json());
-    // Run each subgraph on the same http server, but at different paths
-    for (const subgraph of this.subgraphs) {
-      const subgraphConfig = this.#getLocalSubgraphConfig(subgraph.name);
-      if (!subgraphConfig) continue;
-      console.log(`Setting up subgraph ${subgraphConfig.name}`);
-      // get schema
-      const schema = createSchema(
-        this.reactor,
-        subgraphConfig.resolvers as GraphQLResolverMap<Context>,
-        subgraphConfig.typeDefs,
-      );
-      // create apollo server
-      const server = new ApolloServer({
-        schema,
-        introspection: true,
-        plugins: [ApolloServerPluginInlineTraceDisabled()],
-      });
-
-      // start apollo server
-      await server.start();
-
-      // setup path
-      const path = `/${subgraphConfig.name}`;
-      newRouter.use(
-        path,
-        // @ts-ignore
-        expressMiddleware(server, {
-          context: ({ req }): Context => ({
-            headers: req.headers,
-            driveId: req.params.drive ?? undefined,
-            driveServer: this.reactor,
-            // analyticStore: undefined, // TODO: add analytic store
-            ...this.getAdditionalContextFields(),
-          }),
-        }),
-      );
-    }
-
-    this.reactorRouter = newRouter;
-    console.log("Router updated.");
+  async #createSubgraphMiddleware(subgraph: Subgraph) {
+    console.log(
+      `Creating subgraph middleware for ${path.join(this.path, subgraph.name)}`,
+    );
+    const schema = createSchema(
+      this.reactor,
+      subgraph.resolvers as GraphQLResolverMap<Context>,
+      subgraph.typeDefs,
+    );
+    const server = new ApolloServer({
+      schema,
+      introspection: true,
+      plugins: [ApolloServerPluginInlineTraceDisabled()],
+    });
+    await server.start();
+    // @ts-expect-error
+    return expressMiddleware(server, {
+      context: ({ req }): Context => ({
+        headers: req.headers,
+        driveId: req.params.drive ?? undefined,
+        driveServer: this.reactor,
+        ...this.getAdditionalContextFields(),
+      }),
+    });
   }
 
   async registerSubgraph(subgraph: Subgraph) {
-    this.subgraphs.unshift(subgraph);
+    this.reactorRouter.use(
+      path.join(this.path, subgraph.name),
+      // @ts-expect-error
+      this.#createSubgraphMiddleware(subgraph),
+    );
     console.log(`Registered [${subgraph.name}] subgraph.`);
     return Promise.resolve();
-  }
-
-  #getLocalSubgraphConfig(subgraphName: string) {
-    return this.subgraphs.find((it) => it.name === subgraphName);
   }
 
   getAdditionalContextFields = () => {
--- packages/reactor-api/src/server.ts
+++ packages/reactor-api/src/server.ts
@@ -42,6 +42,7 @@ export async function startAPI(
     dataSources: {
       db: {
         Analytics: new AnalyticsModel(new AnalyticsQueryEngine(analyticsStore)),
+        Operational: knex,
       },
     },
   });
