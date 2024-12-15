aac4a46c641b2afbd9ba52016c244c7e28087cdc
Frank <frank.pfeift@protonmail.com>
2024-12-15 19:33:54 +0100

    chore(reactor-local): optimized listener warning

--- packages/reactor-local/src/index.ts
+++ packages/reactor-local/src/index.ts
@@ -1,6 +1,6 @@
-import { startServer } from "./server.js";
+import { startServer } from "./server";
 
-export * from "./server.js";
+export * from "./server";
 
 startServer({ dev: true }).catch((error: unknown) => {
   throw error;
