3471fd1657b35256da4b9aded965c06e705b007c
Yasiel Cabrera <yasiel9506@gmail.com>
2024-12-16 15:28:01 -0500

    feat(scalars): add url platform icons

---
+++ packages/design-system/src/scalars/components/url-field/url-favicon.tsx
@@ -0,0 +1,47 @@
+import { IconName, iconNames } from "@/powerhouse";
+import { Icon } from "@/powerhouse/components/icon";
+// import { type PlatformIcon } from "./url-field";
+import { useMemo } from "react";
+
+interface UrlFaviconProps {
+  url: string;
+  platformIcons?: Record<string, React.ReactElement | IconName>;
+}
+
+const DEFAULT_ICON_NAME = "GlobeWww";
+
+const UrlFavicon = ({ url, platformIcons }: UrlFaviconProps) => {
+  const IconToUse: React.ReactElement | null = useMemo(() => {
+    const defaultIcon = <Icon name={DEFAULT_ICON_NAME} size={18} />;
+    try {
+      const parsedUrl = new URL(url);
+      const hostname = parsedUrl.hostname;
+      const icon = platformIcons?.[hostname];
+      if (!icon) return defaultIcon;
+
+      if (typeof icon === "string") {
+        // check beyond typescript check if the icon actually exists
+        if (iconNames.includes(icon)) {
+          return <Icon name={icon} size={18} />;
+        } else {
+          console.error(`Platform icon "${icon}" does not exist`);
+          return defaultIcon;
+        }
+      }
+
+      return icon;
+    } catch {
+      return defaultIcon;
+    }
+  }, [url, platformIcons]);
+
+  if (Object.keys(platformIcons ?? {}).length === 0) return null;
+
+  return (
+    <div className="absolute left-2.5 top-0 flex h-full items-center justify-center text-gray-900">
+      {IconToUse}
+    </div>
+  );
+};
+
+export default UrlFavicon;
