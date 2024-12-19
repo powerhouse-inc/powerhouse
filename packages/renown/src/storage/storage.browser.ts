2864fbc620c165d085fe14b45c8e7d132a08f535
acaldas <acaldas.dev@gmail.com>
2024-12-19 15:46:51 +0000

    feat(renown): added initial renown sdk package

---
+++ packages/renown/src/storage/storage.browser.ts
@@ -0,0 +1,30 @@
+import { BaseStorage } from "./common";
+
+export class BrowserStorage<
+  T extends Record<string, unknown> = Record<string, unknown>,
+> extends BaseStorage<T> {
+  constructor(namespace: string, basename: string | undefined) {
+    super(`${basename}:${namespace}`);
+  }
+
+  #buildKey(key: keyof T): string {
+    return `${this.namespace}:${key.toString()}`;
+  }
+  get<Key extends keyof T>(key: Key): T[Key] | undefined {
+    const value = localStorage.getItem(this.#buildKey(key));
+    if (value) {
+      return JSON.parse(value) as T[Key];
+    }
+    return undefined;
+  }
+
+  set<Key extends keyof T>(key: Key, value?: T[Key] | undefined): void {
+    return value
+      ? localStorage.setItem(this.#buildKey(key), JSON.stringify(value))
+      : localStorage.removeItem(this.#buildKey(key));
+  }
+
+  delete(key: keyof T): void {
+    return localStorage.removeItem(this.#buildKey(key));
+  }
+}
