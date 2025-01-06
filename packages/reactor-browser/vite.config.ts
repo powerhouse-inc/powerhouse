bceeeccc967659e0de0c36689d080490a9f8a0ef
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2025-01-06 16:32:05 -0300

    feat(react-reactor): updated build config (#759)

--- packages/reactor-browser/vite.config.ts
+++ packages/reactor-browser/vite.config.ts
@@ -1,44 +1,84 @@
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path, { resolve } from "path";
+import { defineConfig, UserConfig } from "vite";
 import dts from "vite-plugin-dts";
-import generateFile from "vite-plugin-generate-file";
 import pkg from "./package.json";
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-    emptyOutDir: false,
-    lib: {
-      entry: ["src/index.ts"],
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: Object.keys(pkg.peerDependencies),
-      output: {
-        entryFileNames: "[format]/[name].js",
-        chunkFileNames: "[format]/internal/[name]-[hash].js",
-        exports: "named",
+const dependencies = Object.keys(pkg.dependencies).concat(
+  Object.keys(pkg.peerDependencies),
+);
+const dependenciesRegex = dependencies.map(
+  (dep) => new RegExp(`^${dep}(?:/.+)?$`),
+);
+const devDependencies = Object.keys(pkg.devDependencies);
+const devDependenciessRegex = devDependencies.map(
+  (dep) => new RegExp(`^${dep}(?:/.+)?$`),
+);
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      dts({
+        include: ["src/**"],
+        exclude: ["src/**/*.stories.*"],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-    sourcemap: true,
-    minify: false,
-  },
-  plugins: [
-    dts({ insertTypesEntry: true, outDir: "dist/types" }),
-    generateFile([
-      {
-        type: "json",
-        output: "./es/package.json",
-        data: {
-          type: "module",
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          chunkFileNames: "[name].[hash].js",
+          assetFileNames: (info) =>
+            // eslint-disable-next-line @typescript-eslint/no-deprecated
+            info.name === "style.css" || info.names.includes("style.css")
+              ? "style.css"
+              : "[name].[hash][extname]",
+          preserveModules: true,
+        },
+        external: (id) => {
+          // include imports using the src alias
+          if (id.startsWith("@/")) {
+            return false;
+          }
+          // exclude all node modules
+          if (id.includes("node_modules")) {
+            return true;
+          }
+
+          // exclude declared dependencies
+          if (
+            dependencies.includes(id) ||
+            dependenciesRegex.some((depMatcher) => depMatcher.test(id))
+          ) {
+            return true;
+          }
+
+          // throw error if dev dependency is included in the bundle
+          if (
+            devDependencies.includes(id) ||
+            devDependenciessRegex.some((depMatcher) => depMatcher.test(id))
+          ) {
+            console.error(
+              `\x1b[31m\n\nDev dependency is being added to the bundle: ${id}\nMove it to dependencies or peerDependecies\n\n`,
+            );
+            process.exit(1);
+          }
         },
       },
-      {
-        type: "json",
-        output: `./cjs/package.json`,
-        data: {
-          type: "commonjs",
+      lib: {
+        entry: [resolve("src", "index.ts")],
+        formats: ["es"],
+        fileName(_format, entryName) {
+          return `${entryName}.js`;
         },
+        cssFileName: "style",
       },
-    ]),
-  ],
+    },
+  } as UserConfig;
 });
