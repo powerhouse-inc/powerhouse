d0b0dd97cdb928dae3105ee9602184cfdcc3f61d
Yasiel Cabrera <yasiel9506@gmail.com>
2025-01-10 14:23:45 -0500

    feat(scalars): sidebar pin items

--- packages/design-system/src/scalars/components/sidebar/subcomponents/sidebar-content-area.tsx
+++ packages/design-system/src/scalars/components/sidebar/subcomponents/sidebar-content-area.tsx
@@ -3,9 +3,14 @@ import { useSidebar } from "./sidebar-provider";
 
 export const SidebarContentArea = () => {
   const { state } = useSidebar();
+  const items =
+    state.pinnedItems.length > 0
+      ? (state.pinnedItems[state.pinnedItems.length - 1].childrens ?? [])
+      : state.items;
+
   return (
     <div className="flex flex-col gap-1 p-2">
-      {state.items.map((item) => (
+      {items.map((item) => (
         <SidebarItem
           key={item.id}
           id={item.id}
