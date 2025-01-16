73165f0ea88c5d2867b65da4767548bce3c93083
Frank <frank.pfeift@protonmail.com>
2025-01-16 09:30:51 +0100

    chore(ph-cli): updated install script

--- packages/ph-cli/scripts/install.sh
+++ packages/ph-cli/scripts/install.sh
@@ -3,6 +3,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm --version
 nvm install 20
-npm install -g @powerhousedao/ph-cli
-npm install -g pm2
-sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
+npm install -g pnpm
+pnpm setup
+pnpm install -g ph-cmd pm2
+OUTPUT=$(pm2 startup | tail -n 1)
+eval $OUTPUT
