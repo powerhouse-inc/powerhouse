4cbae301bd2c6fd7fa8a813f75b3e6375b336232
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2025-01-21 11:33:15 -0300

    feat(ph-cmd): enable support for package scripts (#880)

--- packages/ph-cmd/src/commands/forward.ts
+++ packages/ph-cmd/src/commands/forward.ts
@@ -25,14 +25,52 @@ export const forwardCommand = (
   const packageManager = getPackageManagerFromLockfile(projectInfo.path);
 
   if (options.debug) {
+    console.log("\n>>> forwardScriptCommand arguments:");
+    console.log(">>> packageManager:", packageManager);
+    console.log(">>> projectPath:", projectInfo.path);
+    console.log(">>> args:", args);
+    console.log(">>> isPackageScript:", true);
+  }
+
+  let packageScriptExecuted = false;
+
+  try {
+    forwardPHCommand(
+      packageManager,
+      projectInfo.path,
+      args,
+      true,
+      options.debug,
+    );
+
+    packageScriptExecuted = true;
+  } catch (error) {
+    if (options.debug) {
+      console.log(
+        ">>> failed to forward command as package script, trying as ph command...",
+        error,
+      );
+    }
+  }
+
+  if (options.debug) {
     console.log("\n>>> forwardCommand arguments:");
     console.log(">>> packageManager:", packageManager);
     console.log(">>> projectPath:", projectInfo.path);
     console.log(">>> args:", args);
+    console.log(">>> isPackageScript:", false);
   }
 
   try {
-    forwardPHCommand(packageManager, projectInfo.path, args, options.debug);
+    if (!packageScriptExecuted) {
+      forwardPHCommand(
+        packageManager,
+        projectInfo.path,
+        args,
+        false,
+        options.debug,
+      );
+    }
   } catch (error) {
     console.error("❌ Failed to forward command");
     throw error;
