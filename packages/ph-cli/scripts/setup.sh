23fb46f91bc2052f00c01647b78da7fcf405b43e
Frank <frank.pfeift@protonmail.com>
2025-01-21 21:14:30 +0100

    feat(ph-cli): added service install script

--- packages/ph-cli/scripts/setup.sh
+++ packages/ph-cli/scripts/setup.sh
@@ -3,6 +3,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm --version
 nvm install 20
-npm install -g pnpm
-pnpm setup
-pnpm install -g ph-cmd pm2
+source $HOME/.bashrc
+npm install -g ph-cmd
