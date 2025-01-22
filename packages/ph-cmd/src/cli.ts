bcfaf79c29f4d921da23708540e31f65d3274334
Guillermo Puente <guillermo.ps09@gmail.com>
2025-01-22 12:23:08 -0300

    feat(ph-cmd): enable option to force forward commands as package scripts

--- packages/ph-cmd/src/cli.ts
+++ packages/ph-cmd/src/cli.ts
@@ -7,14 +7,17 @@ import { PH_CLI_COMMANDS } from "./utils.js";
 
 const program = new Command();
 
-const defaultCommand: CommandActionType<[{ verbose?: boolean }]> = (
-  options,
-) => {
+const defaultCommand: CommandActionType<
+  [{ verbose?: boolean; script?: boolean }]
+> = (options) => {
   const allArgs = process.argv.slice(2);
-  const filteredArgs = allArgs.filter((arg) => arg !== "--verbose");
+  const filteredArgs = allArgs
+    .filter((arg) => arg !== "--verbose")
+    .filter((arg) => arg !== "--script");
   const command = filteredArgs.at(0);
 
-  const isPackageScript = !PH_CLI_COMMANDS.includes(command ?? "");
+  const isPackageScript =
+    options.script ?? !PH_CLI_COMMANDS.includes(command ?? "");
   const args = filteredArgs.join(" ");
 
   forwardCommand(args, { debug: !!options.verbose, isPackageScript });
@@ -25,6 +28,7 @@ program
   .description("CLI tool for Powerhouse DAO")
   .allowUnknownOption()
   .option("--verbose", "Enable debug mode")
+  .option("--script", "Run the command as a package.json script")
   .action(defaultCommand)
   .version("1.0.0");
 
