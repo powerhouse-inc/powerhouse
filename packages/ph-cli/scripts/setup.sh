960b0e495a29a4a01c85715d21299eee0a29db1d
Frank <frank.pfeift@protonmail.com>
2025-01-22 15:24:54 +0100

    fix: improved setup script

--- packages/ph-cli/scripts/setup.sh
+++ packages/ph-cli/scripts/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm --version
 nvm install 20
+npm install -g pnpm
+pnpm setup
 source $HOME/.bashrc
-npm install -g ph-cmd
+pnpm install -g ph-cmd
