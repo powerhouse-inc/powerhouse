850ca1bf1e167ce34932a9531a0b759a59bd39c7
Frank <frank.pfeift@protonmail.com>
2025-01-23 15:57:32 +0100

    refactor(ph-cli): pm2-scripts

--- packages/ph-cli/scripts/service-startup.sh
+++ packages/ph-cli/scripts/service-startup.sh
@@ -1,4 +1,6 @@
 OUTPUT=$(pm2 startup | tail -n 1)
 eval $OUTPUT
+ps aux | grep pm2 | grep -v grep | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
+sudo systemctl daemon-reload
 sudo systemctl enable pm2-$USER
 sudo systemctl start pm2-$USER
