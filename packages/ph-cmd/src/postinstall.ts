9ff15eb9caf0e0bd86f293ad14c09ee85ac3d857
Guillermo Puente <guillermo.ps09@gmail.com>
2025-01-23 15:19:57 -0300

    fix(ph-cmd): postinstall script

--- packages/ph-cmd/src/postinstall.ts
+++
@@ -1,7 +0,0 @@
-import { init } from "./commands/setup-globals.js";
-
-const createGlobalProject = async () => {
-  await init(undefined, {});
-};
-
-createGlobalProject().catch((err: unknown) => console.error(err));
