0516b2e7f6296ea0299ec3c4b3262b9e2bb2b743
ryanwolhuter <dev@ryanwolhuter.com>
2025-01-24 12:00:32 +0200

    feat(document-model-libs): remove rwa
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/document-model-libs/document-models/real-world-assets/src/_utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./calculations";
-export * from "./diff";
-export * from "./generators";
-export * from "./getters";
-export * from "./validators";
---
+++ packages/reactor-api/src/subgraphs/drive/temp-hack-rwa-type-defs.ts
@@ -0,0 +1,39 @@
+import { Maybe } from "document-model/document-model";
+
+export type Scalars = {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  DateTime: { input: string; output: string };
+};
+export type Asset = Cash | FixedIncome;
+
+export type AssetType = "Cash" | "FixedIncome";
+export type Cash = {
+  balance: Scalars["Float"]["output"];
+  currency: Scalars["String"]["output"];
+  id: Scalars["ID"]["output"];
+  spvId: Scalars["ID"]["output"];
+  type: AssetType | `${AssetType}`;
+};
+export type FixedIncome = {
+  CUSIP: Maybe<Scalars["String"]["output"]>;
+  ISIN: Maybe<Scalars["String"]["output"]>;
+  assetProceeds: Scalars["Float"]["output"];
+  coupon: Maybe<Scalars["Float"]["output"]>;
+  fixedIncomeTypeId: Scalars["ID"]["output"];
+  id: Scalars["ID"]["output"];
+  maturity: Maybe<Scalars["DateTime"]["output"]>;
+  name: Scalars["String"]["output"];
+  notional: Scalars["Float"]["output"];
+  purchaseDate: Scalars["DateTime"]["output"];
+  purchasePrice: Scalars["Float"]["output"];
+  purchaseProceeds: Scalars["Float"]["output"];
+  realizedSurplus: Scalars["Float"]["output"];
+  salesProceeds: Scalars["Float"]["output"];
+  spvId: Scalars["ID"]["output"];
+  totalDiscount: Scalars["Float"]["output"];
+  type: AssetType | `${AssetType}`;
+};
