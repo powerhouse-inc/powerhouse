acccebb4f168c83a43750921b884adbe636328bd
Frank <frank.pfeift@protonmail.com>
2025-01-27 13:09:30 +0100

    chore(ph-cli): changed default node version to 22

--- packages/ph-cli/scripts/setup.sh
+++ packages/ph-cli/scripts/setup.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm --version
-nvm install 20
+nvm install 22
 npm install -g pnpm
 pnpm setup
 source $HOME/.bashrc
