331c9dd2410600abca905d72f7216c007f081c8b
Guillermo Puente <guillermo.ps09@gmail.com>
2025-01-27 10:01:50 -0300

    fix(ph-cmd): inject ph-cmd version in build time

--- packages/ph-cmd/tsup.config.ts
+++ packages/ph-cmd/tsup.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from "tsup";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+
+const packageJsonPath = path.resolve(__dirname, "package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8")) as {
+  version: string;
+};
+
+const appVersion = packageJson.version;
 
 export default defineConfig({
   entry: ["src/**/*.ts"],
@@ -12,4 +21,7 @@ export default defineConfig({
   target: "node20",
   cjsInterop: true,
   dts: true,
+  define: {
+    "process.env.APP_VERSION": JSON.stringify(appVersion),
+  },
 });
