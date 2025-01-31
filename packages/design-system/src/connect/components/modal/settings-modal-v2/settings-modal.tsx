1ebd73d651bbbe86c21024b70a0fc07ee431547e
Guillermo Puente <guillermo.ps09@gmail.com>
2025-01-31 11:58:23 -0300

    feat(design-system): added base settings modal

---
+++ packages/design-system/src/connect/components/modal/settings-modal-v2/settings-modal.tsx
@@ -0,0 +1,83 @@
+import { Icon, Modal } from "@/powerhouse";
+import React, { ComponentPropsWithoutRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type SettingsTab = {
+  id: string;
+  icon?: React.ReactNode;
+  label: React.ReactNode;
+  content: React.ReactNode;
+};
+
+export type SettingsModalProps = ComponentPropsWithoutRef<typeof Modal> & {
+  readonly title: React.ReactNode;
+  readonly tabs: SettingsTab[];
+};
+
+export const SettingsModal: React.FC<SettingsModalProps> = (props) => {
+  const {
+    title,
+    overlayProps,
+    contentProps,
+    onOpenChange,
+    tabs,
+    ...restProps
+  } = props;
+
+  const initialTabId = tabs[0].id;
+  const [selectedTab, setSelectedTab] = useState(initialTabId);
+
+  const tabsContent = tabs.map((tab) => (
+    <button onClick={() => setSelectedTab(tab.id)}>
+      <div
+        key={tab.id}
+        className={twMerge(
+          "flex h-9 w-48 cursor-pointer items-center gap-x-2 rounded-md pl-3 hover:bg-slate-50",
+          selectedTab === tab.id ? "bg-slate-50" : "bg-transparent",
+        )}
+      >
+        {tab.icon}
+        <span>{tab.label}</span>
+      </div>
+    </button>
+  ));
+
+  const selectedTabContent = tabs.find(
+    (tab) => tab.id === selectedTab,
+  )?.content;
+
+  return (
+    <Modal
+      contentProps={{
+        ...contentProps,
+        className: twMerge("rounded-xl", contentProps?.className),
+      }}
+      onOpenChange={onOpenChange}
+      overlayProps={{
+        ...overlayProps,
+        className: twMerge("top-10", overlayProps?.className),
+      }}
+      {...restProps}
+    >
+      <div className="flex flex-col text-gray-900 h-screen w-screen max-w-[900px] max-h-[700px]">
+        <div className="flex justify-between border-b border-slate-50 p-4">
+          <h1 className="text-center text-xl font-semibold">{title}</h1>
+          <button
+            className="flex size-6 items-center justify-center rounded-md outline-none"
+            onClick={() => onOpenChange?.(false)}
+          >
+            <Icon name="XmarkLight" size={24} />
+          </button>
+        </div>
+        <div className="flex flex-1 overflow-hidden">
+          <div className="flex flex-col gap-y-1 p-3 pt-6">{tabsContent}</div>
+          <div className="m-6   max-h-full  flex-1">
+            <div className="max-h-full overflow-y-auto rounded-lg border border-slate-50 bg-gray-50 p-3">
+              {selectedTabContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
