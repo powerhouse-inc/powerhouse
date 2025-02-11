de4b5ebe61094df7a42dc9198ba218ecdc998479
acaldas <acaldas.dev@gmail.com>
2025-02-11 11:15:48 +0000

    feat(connect,design-system): add immutable version of package manager

--- apps/connect/src/store/external-packages.ts
+++ apps/connect/src/store/external-packages.ts
@@ -1,7 +1,7 @@
 import { DocumentModelLib } from 'document-model/document';
-import { useAtomValue } from 'jotai';
+import { atom, useAtomValue } from 'jotai';
 import { atomWithLazy } from 'jotai/utils';
-import { subscribeExternalPackages } from 'src/services/hmr';
+import { getHMRModule, subscribeExternalPackages } from 'src/services/hmr';
 
 const LOAD_EXTERNAL_PACKAGES = import.meta.env.LOAD_EXTERNAL_PACKAGES;
 const shouldLoadExternalPackages = LOAD_EXTERNAL_PACKAGES === 'true';
@@ -17,6 +17,13 @@ function loadExternalPackages() {
         );
     }
 }
+
+const hmrAvailableAtom = atom(async () => {
+    const module = await getHMRModule();
+    return typeof module !== 'undefined';
+});
+
+export const useMutableExternalPackages = () => useAtomValue(hmrAvailableAtom);
 
 // fetches the initial external packages only when needed
 export const externalPackagesAtom = atomWithLazy(loadExternalPackages);
