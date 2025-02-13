80eb482dc1b3d07697ff7af24143ab0d98011691
Frank <frank.pfeift@protonmail.com>
2025-02-13 15:38:25 +0100

    feat(connect): added ph-cli var

--- apps/connect/src/components/modal/modals/settings/about.tsx
+++ apps/connect/src/components/modal/modals/settings/about.tsx
@@ -1,6 +1,17 @@
 import { About as BaseAbout } from '@powerhousedao/design-system';
+import { useConnectConfig } from 'src/hooks/useConnectConfig';
 import packageJson from '../../../../../package.json';
 
 export const About: React.FC = () => {
-    return <BaseAbout packageJson={packageJson} />;
+    const [connectConfig] = useConnectConfig();
+    return (
+        <BaseAbout
+            packageJson={packageJson}
+            phCliVersion={
+                typeof connectConfig.phCliVersion === 'string'
+                    ? connectConfig.phCliVersion
+                    : undefined
+            }
+        />
+    );
 };
