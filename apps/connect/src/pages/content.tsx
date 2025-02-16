0a6620aaa45f7e3f2451ea66bda13ec3c44bde59
acaldas <acaldas.dev@gmail.com>
2025-02-16 14:15:20 +0000

    refactor(connect): move code from content page to drive view container

--- apps/connect/src/pages/content.tsx
+++ apps/connect/src/pages/content.tsx
@@ -1,39 +1,16 @@
-import { Breadcrumbs, FILE } from '@powerhousedao/design-system';
-import { DocumentModel } from 'document-model/document';
+import { FILE } from '@powerhousedao/design-system';
 import { useEffect } from 'react';
-import Button from 'src/components/button';
 import { DocumentEditorContainer } from 'src/components/document-editor-container';
-import FolderView from 'src/components/folder-view';
+import { DriveView } from 'src/components/drive-view';
 import { Footer } from 'src/components/footer';
-import { useModal } from 'src/components/modal';
-import { SearchBar } from 'src/components/search-bar';
-import { useConnectConfig } from 'src/hooks/useConnectConfig';
 import { useNodeNavigation } from 'src/hooks/useNodeNavigation';
 import { useUiNodes } from 'src/hooks/useUiNodes';
 
-const getDocumentModelName = (name: string) => {
-    if (name === 'RealWorldAssets') {
-        return 'RWA Portfolio';
-    }
-
-    return name;
-};
-
 export default function Content() {
-    const [connectConfig] = useConnectConfig();
     const uiNodes = useUiNodes();
-    const {
-        fileNodeDocument,
-        documentModels,
-        isAllowedToCreateDocuments,
-        selectedDriveNode,
-        selectedParentNode,
-        setSelectedNode,
-        selectedNode,
-        addFile,
-    } = useUiNodes();
+    const { fileNodeDocument, selectedDriveNode, selectedNode, addFile } =
+        uiNodes;
     useNodeNavigation();
-    const { showModal } = useModal();
 
     useEffect(() => {
         return window.electronAPI?.handleFileOpen(async file => {
@@ -50,16 +27,6 @@ export default function Content() {
         });
     }, [selectedDriveNode, selectedNode, addFile]);
 
-    function createDocument(documentModel: DocumentModel) {
-        if (!selectedDriveNode) return;
-
-        showModal('createDocument', {
-            documentModel,
-            selectedParentNode,
-            setSelectedNode,
-        });
-    }
-
     return (
         <div
             className="flex h-full flex-col overflow-auto bg-gray-100 p-6 pb-3"
@@ -68,40 +35,7 @@ export default function Content() {
             {fileNodeDocument ? (
                 <DocumentEditorContainer key={fileNodeDocument.documentId} />
             ) : (
-                <div className="grow overflow-auto rounded-2xl bg-gray-50 p-2">
-                    <Breadcrumbs {...uiNodes} />
-                    {connectConfig.content.showSearchBar && <SearchBar />}
-                    <div className="px-4">
-                        <div className="mb-5">
-                            <FolderView {...uiNodes} />
-                        </div>
-                        {isAllowedToCreateDocuments && (
-                            <>
-                                <h3 className="mb-3 mt-4 text-xl font-bold text-gray-600">
-                                    New document
-                                </h3>
-                                <div className="flex w-full flex-wrap gap-4">
-                                    {documentModels?.map(doc => (
-                                        <Button
-                                            key={doc.documentModel.id}
-                                            aria-details={
-                                                doc.documentModel.description
-                                            }
-                                            className="bg-gray-200 text-slate-800"
-                                            onClick={() => createDocument(doc)}
-                                        >
-                                            <span className="text-sm">
-                                                {getDocumentModelName(
-                                                    doc.documentModel.name,
-                                                )}
-                                            </span>
-                                        </Button>
-                                    ))}
-                                </div>
-                            </>
-                        )}
-                    </div>
-                </div>
+                <DriveView />
             )}
             <div className="flex w-full flex-row justify-end pr-3 pt-3">
                 <Footer />
