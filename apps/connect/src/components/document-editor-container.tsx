720b03bc71296eefc6a1ae96919883cf7012b680
acaldas <acaldas.dev@gmail.com>
2025-02-16 13:52:54 +0000

    refactor(connect): move code from content page to document editor container

---
+++ apps/connect/src/components/document-editor-container.tsx
@@ -0,0 +1,135 @@
+import { Document, Operation } from 'document-model/document';
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useModal } from '../components/modal';
+import { useUiNodes } from '../hooks/useUiNodes';
+import { exportFile } from '../utils';
+import { validateDocument } from '../utils/validate-document';
+import { DocumentEditor } from './editors';
+
+export function DocumentEditorContainer() {
+    const { t } = useTranslation();
+    const { showModal } = useModal();
+    const {
+        selectedNode,
+        selectedParentNode,
+        isRemoteDrive,
+        selectedDocument,
+        fileNodeDocument,
+        setSelectedNode,
+        setSelectedDocument,
+        openSwitchboardLink,
+        addOperationToSelectedDocument,
+        renameNode,
+        getDocumentModel,
+    } = useUiNodes();
+
+    const handleAddOperationToSelectedDocument = useCallback(
+        async (operation: Operation) => {
+            if (!selectedDocument) {
+                throw new Error('No document selected');
+            }
+            if (!addOperationToSelectedDocument) {
+                throw new Error('No add operation function defined');
+            }
+            await addOperationToSelectedDocument(operation);
+        },
+        [addOperationToSelectedDocument, selectedDocument],
+    );
+
+    const onDocumentChangeHandler = useCallback(
+        (documentId: string, document: Document) => {
+            if (documentId !== fileNodeDocument?.documentId) {
+                return;
+            }
+            setSelectedDocument(document);
+
+            if (
+                !!selectedNode &&
+                document.name !== '' &&
+                selectedNode.name !== document.name
+            ) {
+                return renameNode(
+                    selectedNode.driveId,
+                    selectedNode.id,
+                    document.name,
+                );
+            }
+        },
+        [
+            fileNodeDocument?.documentId,
+            renameNode,
+            selectedNode,
+            setSelectedDocument,
+        ],
+    );
+
+    const onClose = useCallback(() => {
+        setSelectedNode(selectedParentNode);
+    }, [selectedParentNode, setSelectedNode]);
+
+    const exportDocument = useCallback(
+        (document: Document) => {
+            const validationErrors = validateDocument(document);
+
+            if (validationErrors.length) {
+                showModal('confirmationModal', {
+                    title: t('modals.exportDocumentWithErrors.title'),
+                    body: (
+                        <div>
+                            <p>{t('modals.exportDocumentWithErrors.body')}</p>
+                            <ul className="mt-4 flex list-disc flex-col items-start px-4 text-xs">
+                                {validationErrors.map((error, index) => (
+                                    <li key={index}>{error.message}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ),
+                    cancelLabel: t('common.cancel'),
+                    continueLabel: t('common.export'),
+                    onCancel(closeModal) {
+                        closeModal();
+                    },
+                    onContinue(closeModal) {
+                        closeModal();
+                        return exportFile(document, getDocumentModel);
+                    },
+                });
+            } else {
+                return exportFile(document, getDocumentModel);
+            }
+        },
+        [getDocumentModel, showModal, t],
+    );
+
+    const onExport = useCallback(() => {
+        if (selectedDocument) {
+            return exportDocument(selectedDocument);
+        }
+    }, [exportDocument, selectedDocument]);
+
+    const onOpenSwitchboardLink = useMemo(() => {
+        return isRemoteDrive
+            ? () => openSwitchboardLink(selectedNode)
+            : undefined;
+    }, [isRemoteDrive, openSwitchboardLink, selectedNode]);
+
+    if (!fileNodeDocument) return null;
+
+    return (
+        <div
+            key={fileNodeDocument.documentId}
+            className="flex-1 rounded-2xl bg-gray-50 p-4"
+        >
+            <DocumentEditor
+                fileNodeDocument={fileNodeDocument}
+                document={selectedDocument}
+                onChange={onDocumentChangeHandler}
+                onClose={onClose}
+                onExport={onExport}
+                onAddOperation={handleAddOperationToSelectedDocument}
+                onOpenSwitchboardLink={onOpenSwitchboardLink}
+            />
+        </div>
+    );
+}
--- apps/connect/src/pages/content.tsx
+++ apps/connect/src/pages/content.tsx
@@ -1,9 +1,8 @@
 import { Breadcrumbs, FILE } from '@powerhousedao/design-system';
-import { Document, DocumentModel, Operation } from 'document-model/document';
-import { useCallback, useEffect, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { DocumentModel } from 'document-model/document';
+import { useEffect } from 'react';
 import Button from 'src/components/button';
-import { DocumentEditor } from 'src/components/editors';
+import { DocumentEditorContainer } from 'src/components/document-editor-container';
 import FolderView from 'src/components/folder-view';
 import { Footer } from 'src/components/footer';
 import { useModal } from 'src/components/modal';
@@ -11,8 +10,6 @@ import { SearchBar } from 'src/components/search-bar';
 import { useConnectConfig } from 'src/hooks/useConnectConfig';
 import { useNodeNavigation } from 'src/hooks/useNodeNavigation';
 import { useUiNodes } from 'src/hooks/useUiNodes';
-import { exportFile } from 'src/utils';
-import { validateDocument } from 'src/utils/validate-document';
 
 const getDocumentModelName = (name: string) => {
     if (name === 'RealWorldAssets') {
@@ -24,24 +21,16 @@ const getDocumentModelName = (name: string) => {
 
 export default function Content() {
     const [connectConfig] = useConnectConfig();
-    const { t } = useTranslation();
     const uiNodes = useUiNodes();
     const {
-        selectedNode,
+        fileNodeDocument,
+        documentModels,
+        isAllowedToCreateDocuments,
         selectedDriveNode,
         selectedParentNode,
-        isRemoteDrive,
-        isAllowedToCreateDocuments,
-        fileNodeDocument,
-        selectedDocument,
-        documentModels,
         setSelectedNode,
-        openSwitchboardLink,
-        setSelectedDocument,
-        addOperationToSelectedDocument,
+        selectedNode,
         addFile,
-        renameNode,
-        getDocumentModel,
     } = useUiNodes();
     useNodeNavigation();
     const { showModal } = useModal();
@@ -61,19 +50,6 @@ export default function Content() {
         });
     }, [selectedDriveNode, selectedNode, addFile]);
 
-    const handleAddOperationToSelectedDocument = useCallback(
-        async (operation: Operation) => {
-            if (!selectedDocument) {
-                throw new Error('No document selected');
-            }
-            if (!addOperationToSelectedDocument) {
-                throw new Error('No add operation function defined');
-            }
-            await addOperationToSelectedDocument(operation);
-        },
-        [addOperationToSelectedDocument, selectedDocument],
-    );
-
     function createDocument(documentModel: DocumentModel) {
         if (!selectedDriveNode) return;
 
@@ -84,100 +60,13 @@ export default function Content() {
         });
     }
 
-    const exportDocument = useCallback(
-        (document: Document) => {
-            const validationErrors = validateDocument(document);
-
-            if (validationErrors.length) {
-                showModal('confirmationModal', {
-                    title: t('modals.exportDocumentWithErrors.title'),
-                    body: (
-                        <div>
-                            <p>{t('modals.exportDocumentWithErrors.body')}</p>
-                            <ul className="mt-4 flex list-disc flex-col items-start px-4 text-xs">
-                                {validationErrors.map((error, index) => (
-                                    <li key={index}>{error.message}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ),
-                    cancelLabel: t('common.cancel'),
-                    continueLabel: t('common.export'),
-                    onCancel(closeModal) {
-                        closeModal();
-                    },
-                    onContinue(closeModal) {
-                        closeModal();
-                        return exportFile(document, getDocumentModel);
-                    },
-                });
-            } else {
-                return exportFile(document, getDocumentModel);
-            }
-        },
-        [getDocumentModel, showModal, t],
-    );
-
-    const onOpenSwitchboardLink = useMemo(() => {
-        return isRemoteDrive
-            ? () => openSwitchboardLink(selectedNode)
-            : undefined;
-    }, [isRemoteDrive, openSwitchboardLink, selectedNode]);
-
-    const onDocumentChangeHandler = useCallback(
-        (documentId: string, document: Document) => {
-            if (documentId !== fileNodeDocument?.documentId) {
-                return;
-            }
-            setSelectedDocument(document);
-
-            if (
-                !!selectedNode &&
-                document.name !== '' &&
-                selectedNode.name !== document.name
-            ) {
-                return renameNode(
-                    selectedNode.driveId,
-                    selectedNode.id,
-                    document.name,
-                );
-            }
-        },
-        [
-            fileNodeDocument?.documentId,
-            renameNode,
-            selectedNode,
-            setSelectedDocument,
-        ],
-    );
-
-    const onClose = useCallback(() => {
-        setSelectedNode(selectedParentNode);
-    }, [selectedParentNode, setSelectedNode]);
-
-    const onExport = useCallback(() => {
-        if (selectedDocument) {
-            return exportDocument(selectedDocument);
-        }
-    }, [exportDocument, selectedDocument]);
-
     return (
         <div
             className="flex h-full flex-col overflow-auto bg-gray-100 p-6 pb-3"
             id="content-view"
         >
             {fileNodeDocument ? (
-                <div className="flex-1 rounded-2xl bg-gray-50 p-4">
-                    <DocumentEditor
-                        fileNodeDocument={fileNodeDocument}
-                        document={selectedDocument}
-                        onChange={onDocumentChangeHandler}
-                        onClose={onClose}
-                        onExport={onExport}
-                        onAddOperation={handleAddOperationToSelectedDocument}
-                        onOpenSwitchboardLink={onOpenSwitchboardLink}
-                    />
-                </div>
+                <DocumentEditorContainer key={fileNodeDocument.documentId} />
             ) : (
                 <div className="grow overflow-auto rounded-2xl bg-gray-50 p-2">
                     <Breadcrumbs {...uiNodes} />
