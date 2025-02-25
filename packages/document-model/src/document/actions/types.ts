a9d06964acca35943a271df88711f50769e35e1c
ryanwolhuter <dev@ryanwolhuter.com>
2025-02-25 16:40:07 +0200

    feat(document-model): re-add missing type arg
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/document-model/src/document/actions/types.ts
+++ packages/document-model/src/document/actions/types.ts
@@ -4,6 +4,7 @@ import type {
   RedoAction as _RedoAction,
   SetNameAction as _SetNameAction,
   UndoAction as _UndoAction,
+  NOOPAction as _NOOPAction,
 } from "../schema/types.js";
 import type { BaseAction } from "../types.js";
 
@@ -19,7 +20,7 @@ export type PruneAction = BaseAction<"PRUNE", PruneActionInput>;
 export type RedoAction = BaseAction<"REDO", _RedoAction["input"]>;
 export type SetNameAction = BaseAction<"SET_NAME", _SetNameAction["input"]>;
 export type UndoAction = BaseAction<"UNDO", _UndoAction["input"]>;
-export type NOOPAction = BaseAction<"NOOP", undefined>;
+export type NOOPAction = BaseAction<"NOOP", _NOOPAction["input"]>;
 
 export type DocumentAction =
   | LoadStateAction
