9e0d86161c4a51e48c85a96d253ccf93133a3b5e
acaldas <acaldas.dev@gmail.com>
2025-03-02 09:35:49 +0000

    ci: ignore codegen hygen package.json

--- packages/design-system/src/powerhouse/style-guide/style-guide.stories.tsx
+++ packages/design-system/src/powerhouse/style-guide/style-guide.stories.tsx
@@ -12,7 +12,7 @@ export const Colors: StoryObj<typeof meta> = {
   render: function Wrapper() {
     return (
       <div className="mx-auto flex max-w-[650px] flex-wrap gap-1 bg-white p-6">
-        {Object.keys(twConfig.theme.colors).map((key) => {
+        {Object.keys(twConfig.theme.extend.colors).map((key) => {
           const colorVar = `--${key}`;
 
           return (
