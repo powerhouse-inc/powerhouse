edda947037bcf70e40c2377040c31d7c3b482975
ryanwolhuter <dev@ryanwolhuter.com>
2025-03-04 11:05:49 +0200

    feat(connect): use css based design system theme
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- apps/connect/tailwind.config.ts
+++ apps/connect/tailwind.config.ts
@@ -1,9 +1,7 @@
-import { designSystemPreset } from '@powerhousedao/config/tailwind';
 import type { Config } from 'tailwindcss';
 
 const config = {
     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-    presets: [designSystemPreset],
     theme: {
         extend: {
             height: {
@@ -13,7 +11,7 @@ const config = {
                 'search-bar-width': 'var(--search-bar-width)',
             },
             boxShadow: {
-                button: '0px 2px 4px 0px rgba(0, 0, 0, 0.15)',
+                button: 'var(--shadow-button)',
             },
         },
     },
--- packages/design-system/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config = {
-  darkMode: ["class", '[data-mode="dark"]'],
-} satisfies Config;
-export default config;
