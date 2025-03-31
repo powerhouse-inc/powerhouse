610b33f502f3f69ace44b4d025dd5f20a9938783
Benjamin Jordan <benjamin@thegoldenmule.com>
2025-03-31 12:24:54 -0500

    feat(reactor): added vscode launch config and cli for debugging react-local

---
+++ packages/reactor-local/src/cli.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { getConfig } from "@powerhousedao/config/powerhouse";
+import path from "path";
+import { startServer } from "./server.js";
+
+const config = getConfig(path.join(process.cwd(), "./powerhouse.config.json"));
+
+startServer({
+  configFile: path.join(process.cwd(), "./powerhouse.config.json"),
+  dev: true,
+  ...config.reactor,
+});
