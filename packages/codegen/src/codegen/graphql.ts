feda3de0a9f04fddfbb01388eccc7870200bbfc6
acaldas <acaldas.dev@gmail.com>
2025-04-05 11:16:02 +0100

    test(design-system): added graphql codegen snapshot tests

--- packages/codegen/src/codegen/graphql.ts
+++ packages/codegen/src/codegen/graphql.ts
@@ -9,15 +9,26 @@ const getDirectories = (source: string) =>
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
+export const scalars = {
+  Unknown: "unknown",
+  DateTime: "string",
+  Attachment: "string",
+  Address: "`${string}:0x${string}`",
+  ...(generatorTypeDefs as Record<string, string>),
+};
+
+export const scalarsValidation = {
+  Unknown: "z.unknown()",
+  DateTime: "z.string().datetime()",
+  Attachment: "z.string()",
+  Address:
+    "z.custom<`${string}:0x${string}`>((val) => /^[a-zA-Z0-9]+:0x[a-fA-F0-9]{40}$/.test(val as string))",
+  ...(validationSchema as Record<string, string>),
+};
+
 export const tsConfig: TypeScriptPluginConfig = {
   strictScalars: true,
-  scalars: {
-    Unknown: "unknown",
-    DateTime: "string",
-    Attachment: "string",
-    Address: "`${string}:0x${string}`",
-    ...(generatorTypeDefs as Record<string, string>),
-  },
+  scalars,
   enumsAsTypes: true,
   allowEnumStringTypes: true,
   avoidOptionals: {
@@ -27,6 +38,20 @@ export const tsConfig: TypeScriptPluginConfig = {
   // maybeValue: "T | null | undefined",
   inputMaybeValue: "T | null | undefined",
 };
+
+export const zodConfig: Record<string, unknown> = {
+  ...tsConfig,
+  importFrom: `./types.js`,
+  schema: "zod",
+  useTypeImports: true,
+  scalarSchemas: scalarsValidation,
+  directives: {
+    equals: {
+      value: ["regex", "/^$1$/"],
+    },
+  },
+  withObjectType: true,
+} as const;
 
 export function schemaConfig(
   name: string,
@@ -47,26 +72,7 @@ export function schemaConfig(
     [`${dir}/${name}/gen/schema/zod.ts`]: {
       schema: `${dir}/${name}/schema.graphql`,
       plugins: ["@acaldas/graphql-codegen-typescript-validation-schema"],
-      config: {
-        ...tsConfig,
-        importFrom: `./types.js`,
-        schema: "zod",
-        useTypeImports: true,
-        scalarSchemas: {
-          Unknown: "z.unknown()",
-          DateTime: "z.string().datetime()",
-          Attachment: "z.string()",
-          Address:
-            "z.custom<`${string}:0x${string}`>((val) => /^[a-zA-Z0-9]+:0x[a-fA-F0-9]{40}$/.test(val as string))",
-          ...(validationSchema as Record<string, string>),
-        },
-        directives: {
-          equals: {
-            value: ["regex", "/^$1$/"],
-          },
-        },
-        withObjectType: true,
-      },
+      config: zodConfig,
     },
   };
 }
