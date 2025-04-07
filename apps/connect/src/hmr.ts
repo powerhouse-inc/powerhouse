bce14f76aca9373e5655e4be88bf39c51fcc8a83
acaldas <acaldas.dev@gmail.com>
2025-04-07 14:59:46 +0100

    ci(connect): fixt import.meta.hot typecast

--- apps/connect/src/hmr.ts
+++ apps/connect/src/hmr.ts
@@ -1,5 +1,3 @@
-import type { ViteHotContext } from 'vite-envs/types/hot';
+import type { ViteHotContext } from 'vite/types/hot.js';
 
-// CI fails without type assertion
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export const hmr = import.meta.hot as ViteHotContext | undefined;
