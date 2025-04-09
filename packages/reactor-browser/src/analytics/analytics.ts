0762a36f7299df25cd225bbe55d2bcaf55f94433
acaldas <acaldas.dev@gmail.com>
2025-04-09 10:02:29 +0100

    feat(reactor-browser): added analytics engine support

---
+++ packages/reactor-browser/src/analytics/analytics.ts
@@ -0,0 +1,8 @@
+export * from "@powerhousedao/analytics-engine-browser";
+export * from "@powerhousedao/analytics-engine-core";
+export { DateTime } from "luxon";
+
+// Export the analytics hooks and context
+export * from "./context.js";
+export * from "./hooks.js";
+
