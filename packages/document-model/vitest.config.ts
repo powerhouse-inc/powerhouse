84f2bf679d85cb11dc5ff876ec992719c5ec15e8
acaldas <acaldas.dev@gmail.com>
2025-04-09 08:31:27 +0100

    test(document-model): run tests on src files

--- packages/document-model/vitest.config.ts
+++ packages/document-model/vitest.config.ts
@@ -1,7 +1,16 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
+
+const srcPath = fileURLToPath(new URL("./src", import.meta.url));
 
 export default defineConfig({
   test: {
     globals: true,
+  },
+  resolve: {
+    alias: [
+      { find: "#utils/env", replacement: `${srcPath}/document/utils/node.ts` },
+      { find: /^#(.*)$/, replacement: `${srcPath}/$1` },
+    ],
   },
 });
