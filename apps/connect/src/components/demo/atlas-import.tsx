c589395ea2b6779bea6728d87db6f97baf8c720d
acaldas <acaldas.dev@gmail.com>
2025-04-09 16:54:59 +0100

    fix(connect): use staging atlas switchboard

--- apps/connect/src/components/demo/atlas-import.tsx
+++ apps/connect/src/components/demo/atlas-import.tsx
@@ -7,8 +7,7 @@ import { gql, request } from 'graphql-request';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
-const REACTOR_URL =
-    'https://apps.powerhouse.io/develop/powerhouse/switchboard/';
+const REACTOR_URL = 'https://apps.powerhouse.io/sky-atlas/staging/switchboard';
 const MIN_LOADING_TIME = 2000;
 
 function useReactorUrl() {
