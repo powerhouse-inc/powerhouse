38384f27d86d1b6fe17ff10b0ed5967e592083d6
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-05-08 17:12:44 -0300

    feat(scalars): add extra label for boolean and scaldfold for diff

--- packages/design-system/src/scalars/components/fragments/toggle-field/toggle-field.tsx
+++ packages/design-system/src/scalars/components/fragments/toggle-field/toggle-field.tsx
@@ -2,9 +2,12 @@ import {
   Toggle,
   type ToggleProps,
 } from "../../../../ui/components/data-entry/toggle/toggle.js";
+import { type WithDifference } from "../../types.js";
 import { withFieldValidation } from "../with-field-validation/with-field-validation.js";
 
-const ToggleField = withFieldValidation<ToggleProps>(Toggle);
+export type ToggleFieldProps = ToggleProps & WithDifference<boolean>;
+
+const ToggleField = withFieldValidation<ToggleFieldProps>(Toggle);
 
 ToggleField.displayName = "ToggleField";
 
