7cab35e969935d8e827111ae68877e3ce0e21580
Frank <frank.pfeift@protonmail.com>
2025-05-14 21:01:46 +0200

    feat(switchboard): added authentication middleware

--- apps/switchboard/src/server.ts
+++ apps/switchboard/src/server.ts
@@ -111,6 +111,7 @@ export const startSwitchboard = async (
       configFile:
         options.configFile ??
         path.join(process.cwd(), "powerhouse.config.json"),
+      auth: options.auth,
     });
 
     return {
