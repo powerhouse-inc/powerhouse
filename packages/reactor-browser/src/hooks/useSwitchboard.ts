88efbf957238e1ea450c9d509e557f5274955944
acaldas <acaldas.dev@gmail.com>
2025-06-16 10:11:16 +0100

    fix: (wip) remove reactor dependency from useSwitchboard

--- packages/reactor-browser/src/hooks/useSwitchboard.ts
+++ packages/reactor-browser/src/hooks/useSwitchboard.ts
@@ -1,65 +1,65 @@
-import { type IDocumentDriveServer } from "document-drive";
 import { generateDocumentStateQueryFields } from "document-drive/utils/graphql";
+import { type DocumentModelModule } from "document-model";
+import { useMemo } from "react";
 
-export function useSwitchboard(reactor: IDocumentDriveServer) {
-  return {
-    getDriveIdBySlug: async (driveUrl: string, slug: string) => {
-      if (!driveUrl) {
-        return;
-      }
+export async function getDriveIdBySlug(driveUrl: string, slug: string) {
+  if (!driveUrl) {
+    return;
+  }
 
-      const urlParts = driveUrl.split("/");
-      urlParts.pop(); // remove id
-      urlParts.pop(); // remove /d
-      urlParts.push("drives"); // add /drives
-      const drivesUrl = urlParts.join("/");
-      const result = await fetch(drivesUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: `
+  const urlParts = driveUrl.split("/");
+  urlParts.pop(); // remove id
+  urlParts.pop(); // remove /d
+  urlParts.push("drives"); // add /drives
+  const drivesUrl = urlParts.join("/");
+  const result = await fetch(drivesUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
                         query getDriveIdBySlug($slug: String!) {
                             driveIdBySlug(slug: $slug)
                         }
                     `,
-          variables: {
-            slug,
-          },
-        }),
-      });
+      variables: {
+        slug,
+      },
+    }),
+  });
 
-      const data = (await result.json()) as {
-        data: { driveIdBySlug: string };
-      };
+  const data = (await result.json()) as {
+    data: { driveIdBySlug: string };
+  };
 
-      return data.data.driveIdBySlug;
-    },
+  return data.data.driveIdBySlug;
+}
 
-    getSwitchboardGatewayUrl: (driveUrl: string) => {
-      const urlParts = driveUrl.split("/");
-      urlParts.pop(); // remove id
-      urlParts.pop(); // remove /d
-      urlParts.push("graphql"); // add /graphql
-      return urlParts.join("/");
-    },
+export function getSwitchboardGatewayUrl(driveUrl: string) {
+  const urlParts = driveUrl.split("/");
+  urlParts.pop(); // remove id
+  urlParts.pop(); // remove /d
+  urlParts.push("graphql"); // add /graphql
+  return urlParts.join("/");
+}
 
-    getDocumentGraphqlQuery: async (driveId: string, documentId: string) => {
-      const doc = await reactor.getDocument(driveId, documentId);
-      const docModel = reactor
-        .getDocumentModelModules()
-        .find((m) => m.documentModel.id === doc.documentType);
+export function getDocumentGraphqlQuery(
+  documentModels: DocumentModelModule[],
+  documentType: string,
+) {
+  const docModel = documentModels.find(
+    (m) => m.documentModel.id === documentType,
+  );
+  if (!docModel) {
+    throw new Error("Document model not found");
+  }
 
-      if (!docModel) {
-        throw new Error("Document model not found");
-      }
-
-      const stateFields = generateDocumentStateQueryFields(
-        docModel.documentModel,
-        "document",
-      );
-      return `
+  const stateFields = generateDocumentStateQueryFields(
+    docModel.documentModel,
+    "document",
+  );
+  return `
         query getDocument($documentId: String!) {
           ${docModel.documentModel.name} {
             getDocument(id: $documentId) {
@@ -68,6 +68,26 @@ export function useSwitchboard(reactor: IDocumentDriveServer) {
           }
         }
       `;
-    },
-  };
 }
+
+export const useGetSwitchboardLink = (
+  url: string,
+  documentType: string,
+  documentModels: DocumentModelModule[],
+) => {
+  return useMemo(() => {
+    if (!url || !documentType || !documentModels.length) {
+      return null;
+    }
+
+    try {
+      const switchboardUrl = getSwitchboardGatewayUrl(url);
+      const query = getDocumentGraphqlQuery(documentModels, documentType);
+      const encodedQuery = encodeURIComponent(query);
+      return `${switchboardUrl}?query=${encodedQuery}`;
+    } catch (error) {
+      console.warn("Error generating switchboard link", error);
+      return null;
+    }
+  }, [url, documentType, documentModels]);
+};
