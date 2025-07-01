17bbca3bb644f1c34a5374f4de9252fefa01f7c8
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-07-01 12:31:45 -0500

    fix: added test to generate and compile a generated document-model

--- packages/codegen/src/codegen/__tests__/document-model.test.ts
+++ packages/codegen/src/codegen/__tests__/document-model.test.ts
@@ -2,6 +2,7 @@ import { exec } from "node:child_process";
 import { rm } from "node:fs/promises";
 import path from "node:path";
 import { describe, it } from "vitest";
+import { generateSchemas } from "../graphql.js";
 import { generateDocumentModel } from "../hygen.js";
 import { loadDocumentModel } from "../utils.js";
 
@@ -13,7 +14,7 @@ describe("document model", () => {
       "codegen",
       "__tests__",
       "data",
-      "document-models",
+      "billing-statement",
     );
 
     const outPath = path.join(
@@ -27,28 +28,40 @@ describe("document model", () => {
     // make sure to remove the outPath directorys
     await rm(outPath, { recursive: true, force: true });
 
+    await generateSchemas(path.join(srcPath, ".."), {
+      skipFormat: true,
+      outDir: outPath,
+    });
+
     const documentModel = await loadDocumentModel(
       path.join(srcPath, "billing-statement.json"),
     );
-
     await generateDocumentModel(documentModel, outPath, { skipFormat: true });
 
     const promise = new Promise((resolve, reject) => {
+      const output: { stdout: string[]; stderr: string[] } = {
+        stdout: [],
+        stderr: [],
+      };
       const child = exec(
         "npx tsc --project tsconfig.document-model.test.json",
         { cwd: process.cwd() },
       );
       child.stdout?.on("data", (data) => {
-        console.log(data);
+        output.stdout.push(data);
       });
       child.stderr?.on("data", (data) => {
-        console.error(data);
+        output.stderr.push(data);
       });
       child.on("close", (code) => {
         if (code === 0) {
           resolve(true);
         } else {
-          reject(new Error(`tsc failed with code ${code}`));
+          reject(
+            new Error(
+              `tsc failed with code ${code}:\n${output.stdout.join("")}\n${output.stderr.join("")}`,
+            ),
+          );
         }
       });
     });
