84a96c3e7a6dc1c8bc359a6a8148514d5986b2af
Guillermo Puente <guillermo.ps09@gmail.com>
2025-07-03 16:52:25 -0400

    feat: enabled pglite live

--- packages/reactor-browser/src/pglite/index.ts
+++ packages/reactor-browser/src/pglite/index.ts
@@ -5,3 +5,9 @@ export type { PGliteWorkerOptions } from "@electric-sql/pglite/worker";
 export { IdbFs, PGlite } from "@electric-sql/pglite";
 export { PGliteProvider } from "@electric-sql/pglite-react";
 
+// TODO: export only live from electric-sql/pglite/live
+export * from "@electric-sql/pglite/live";
+
+export type { PGliteWithLive } from "./provider.js";
+
+export { PGliteAsyncProvider, usePGliteAsync } from "./provider.js";
