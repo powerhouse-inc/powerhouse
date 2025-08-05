dd63103acd2e2fd3d2148daec6b281e0a101e199
Guillermo Puente <guillermo.ps09@gmail.com>
2025-08-05 13:05:06 -0400

    feat(vetra): enabled support for new documents in codegen processor

--- packages/vetra/processors/codegen/factory.ts
+++ packages/vetra/processors/codegen/factory.ts
@@ -15,7 +15,7 @@ import { CodegenProcessor } from "./index.js";
           filter: {
             branch: ["main"],
             documentId: ["*"],
-            documentType: ["powerhouse/document-model"],
+            documentType: ["powerhouse/document-model", "powerhouse/package", "powerhouse/document-editor"],
             scope: ["global"],
           },
         },
--- packages/vetra/processors/codegen/index.ts
+++ packages/vetra/processors/codegen/index.ts
@@ -13,18 +13,27 @@ export class CodegenProcessor implements IProcessor {
   async onStrands<TDocument extends DocumentModelDocument>(
     strands: InternalTransmitterUpdate<TDocument>[],
   ): Promise<void> {
+    console.log(">>> onStrands", strands);
     for (const strand of strands) {
-      const state = strand.state as DocumentModelState;
-      const validationResult = validateDocumentModelState(state);
-  
-      if (validationResult.isValid) {
-        console.log(`🔄 Starting code generation for document model: ${state.name}`);
-        try {
-          await generateFromDocument(state, PH_CONFIG, { verbose: false });
-          console.log(`✅ Code generation completed successfully for: ${state.name}`);
-        } catch (error) {
-          console.error(`❌ Error during code generation for ${state.name}:`, error);
+      if (strand.documentType === "powerhouse/document-model") {
+        const state = strand.state as DocumentModelState;
+        const validationResult = validateDocumentModelState(state);
+    
+        if (validationResult.isValid) {
+          console.log(`🔄 Starting code generation for document model: ${state.name}`);
+          try {
+            await generateFromDocument(state, PH_CONFIG, { verbose: false });
+            console.log(`✅ Code generation completed successfully for: ${state.name}`);
+          } catch (error) {
+            console.error(`❌ Error during code generation for ${state.name}:`, error);
+          }
         }
+      } else if (strand.documentType === "powerhouse/package") {
+        console.log(">>> generate powerhouse/package", strand.state);
+      } else if (strand.documentType === "powerhouse/document-editor") {
+        console.log(">>> generate powerhouse/document-editor", strand.state);
+      } else {
+        console.log(">>> unknown document type", strand.documentType);
       }
     }
   }
