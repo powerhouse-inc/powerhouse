f35f019e254867463afe3392b009b50357aeaff5
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-08-06 13:40:20 -0500

    refactor(document-model): pulling another operation factory into ph-factories

--- packages/document-model/src/document/ph-factories.ts
+++ packages/document-model/src/document/ph-factories.ts
@@ -1,4 +1,9 @@
-import { Action, Operation } from "./types.js";
+import {
+  Action,
+  Operation,
+  OperationSignatureContext,
+  Signature,
+} from "./types.js";
 import { generateId } from "./utils/crypto.js";
 
 export const operationFromAction = (
@@ -29,4 +34,58 @@ export const operationFromOperation = (
 
     skip,
   };
+};
+
+export const operationWithSignature = (
+  operation: Operation,
+  signature: Signature,
+): Operation => {
+  if (!operation.action) {
+    throw new Error("Operation has no action");
+  }
+
+  if (!operation.action.context) {
+    throw new Error("Operation has no context");
+  }
+
+  if (!operation.action.context.signer) {
+    throw new Error("Operation has no signer");
+  }
+
+  const action = operation.action;
+  const context = action.context!;
+  const signer = context.signer!;
+  const signatures = signer.signatures ?? [];
+
+  return {
+    ...operation,
+    action: {
+      ...action,
+      context: {
+        ...context,
+        signer: {
+          ...signer,
+          signatures: [...signatures, signature],
+        },
+      },
+    },
+  };
+};
+
+export const operationWithSignatureDeprecated = (
+  operation: Operation,
+  context: Omit<OperationSignatureContext, "operation" | "previousStateHash">,
+  signature: Signature,
+): Operation => {
+  return {
+    ...operation,
+    context: {
+      ...operation.context,
+      signer: {
+        ...operation.context?.signer,
+        ...context.signer,
+        signatures: [...(context.signer.signatures ?? []), signature],
+      },
+    },
+  } as Operation;
 };
