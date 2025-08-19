48e8ed60bc30281c99d271c81cb418f65814b6d0
ryanwolhuter <dev@ryanwolhuter.com>
2025-08-19 12:05:23 +0200

    feat(connect): remove redundant global types
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- apps/connect/src/globals.ts
+++ apps/connect/src/globals.ts
@@ -1,33 +1,9 @@
-import { type UserPermissions } from '@powerhousedao/reactor-browser';
-import {
-    type DID,
-    type IConnectCrypto,
-} from '@powerhousedao/reactor-browser/crypto/index';
-import { type IRenown, type User } from '@renown/sdk';
-import { type IDocumentDriveServer } from 'document-drive';
-import {
-    type IDocumentAdminStorage,
-    type IDocumentOperationStorage,
-    type IDocumentStorage,
-    type IDriveOperationStorage,
-} from 'document-drive/storage/types';
 // import type { ElectronAPI } from './preload.js';
 import type { DocumentEditorDebugTools } from './utils/document-editor-debug-tools.js';
 
 declare global {
     interface Window {
-        // electronAPI?: ElectronAPI;
-        connectCrypto?: IConnectCrypto;
-        did?: DID;
-        renown?: IRenown;
-        user?: User | undefined;
-
         documentEditorDebugTools?: DocumentEditorDebugTools;
-        reactor?: IDocumentDriveServer;
-        phStorage?: IDriveOperationStorage &
-            IDocumentOperationStorage &
-            IDocumentStorage &
-            IDocumentAdminStorage;
     }
 
     const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
