5768223846690bc17575f37310efbd0acbcc2283
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-08-21 08:42:36 -0500

    refactor(document-drive): update test-factories to use the generated factories

--- packages/document-drive/src/drive-document-model/src/tests/test-factories.ts
+++ packages/document-drive/src/drive-document-model/src/tests/test-factories.ts
@@ -2,40 +2,32 @@
  * Test factory methods for creating DocumentDriveDocument instances with custom state
  */
 
-import { createDocument, createState } from "../../gen/utils.js";
-import type { DocumentDriveDocument } from "../../gen/types.js";
 import type { Node } from "../../gen/schema/types.js";
+import type { DocumentDriveDocument } from "../../gen/types.js";
+import { createDriveDocument } from "../../ph-factories.js";
 
 /**
  * Creates a DocumentDriveDocument with custom nodes in the global state
  */
-export function createDocumentWithNodes(nodes: Partial<Node>[]): DocumentDriveDocument {
-  const document = createDocument();
-  // Directly modify the state after creation
-  document.state.global.nodes = nodes as Node[];
-  document.initialState.global.nodes = nodes as Node[];
-  
-  return document;
+export function createDocumentWithNodes(
+  nodes: Partial<Node>[],
+): DocumentDriveDocument {
+  return createDriveDocument({
+    global: {
+      nodes: nodes as Node[],
+    },
+  });
 }
 
 /**
  * Creates a DocumentDriveDocument with custom global and local state
  */
 export function createDocumentWithState(
-  globalState?: Partial<DocumentDriveDocument['state']['global']>,
-  localState?: Partial<DocumentDriveDocument['state']['local']>
+  globalState?: Partial<DocumentDriveDocument["state"]["global"]>,
+  localState?: Partial<DocumentDriveDocument["state"]["local"]>,
 ): DocumentDriveDocument {
-  const document = createDocument();
-  
-  if (globalState) {
-    Object.assign(document.state.global, globalState);
-    Object.assign(document.initialState.global, globalState);
-  }
-  
-  if (localState) {
-    Object.assign(document.state.local, localState);
-    Object.assign(document.initialState.local, localState);
-  }
-  
-  return document;
+  return createDriveDocument({
+    global: globalState,
+    local: localState,
+  });
 }
