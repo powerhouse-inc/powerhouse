c6ebbe47ca8bf9edcfb26c0aeb44eac5952f7994
acaldas <acaldas.dev@gmail.com>
2025-09-03 18:01:16 +0100

    test: fixed connect-e2e tests

--- test/connect-e2e/tests/helpers/index.ts
+++ test/connect-e2e/tests/helpers/index.ts
@@ -1,6 +1,6 @@
 export * from "./code.js";
 export * from "./cookie-consent.js";
 export * from "./document.js";
+export * from "./drive.js";
 export * from "./folder.js";
 export * from "./navigation.js";
-
