75390008862edd1aae47f489f24a18407abcb35c
ryanwolhuter <dev@ryanwolhuter.com>
2025-09-12 14:35:09 +0200

    feat(monorepo): revert
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- apps/connect/src/components/app.tsx
+++ apps/connect/src/components/app.tsx
@@ -4,26 +4,29 @@ import {
   serviceWorkerManager,
 } from "@powerhousedao/connect";
 import { ToastContainer, WagmiContext } from "@powerhousedao/design-system";
+import { useEffect } from "react";
 import ProcessorManagerProvider from "../context/processor-manager.js";
 import Analytics from "./analytics.js";
 import { Router } from "./router.js";
 
-// if (import.meta.env.MODE === "development") {
-//   window.documentEditorDebugTools = new DocumentEditorDebugTools();
-// } else {
-//   serviceWorkerManager.registerServiceWorker(false);
-// }
-
-const App = () => (
-  <SentryProvider>
-    <WagmiContext>
-      <ProcessorManagerProvider>
-        <ToastContainer position="bottom-right" containerId="connect" />
-        <Router />
-        <Analytics />
-      </ProcessorManagerProvider>
-    </WagmiContext>
-  </SentryProvider>
-);
-
+const App = () => {
+  useEffect(() => {
+    if (import.meta.env.MODE === "development") {
+      window.documentEditorDebugTools = new DocumentEditorDebugTools();
+    } else {
+      serviceWorkerManager.registerServiceWorker(false);
+    }
+  }, []);
+  return (
+    <SentryProvider>
+      <WagmiContext>
+        <ProcessorManagerProvider>
+          <ToastContainer position="bottom-right" containerId="connect" />
+          <Router />
+          <Analytics />
+        </ProcessorManagerProvider>
+      </WagmiContext>
+    </SentryProvider>
+  );
+};
 export default App;
