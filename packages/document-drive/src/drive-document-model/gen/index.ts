bc10c155e8a2a2532b06d828b308325d7342c674
ryanwolhuter <dev@ryanwolhuter.com>
2025-09-15 13:43:12 +0200

    feat(document-drive): revert missing operations interface
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/document-drive/src/drive-document-model/gen/index.ts
+++ packages/document-drive/src/drive-document-model/gen/index.ts
@@ -2,6 +2,7 @@ export * from "./creators.js";
 export * from "./document-model.js";
 export * from "./drive/index.js";
 export * from "./node/index.js";
+export * from "./ph-factories.js";
 export * from "./reducer.js";
 export * from "./schema/index.js";
 export * from "./utils.js";
--- packages/document-drive/src/drive-document-model/types.ts
+++ packages/document-drive/src/drive-document-model/types.ts
@@ -5,6 +5,7 @@ import type {
 import type { DocumentModelModule, PHBaseState } from "document-model";
 
 export type * from "./gen/types.js";
+export type * from "./src/types.js";
 export type DocumentDrivePHState = PHBaseState & {
   global: DocumentDriveGlobalState;
   local: DocumentDriveLocalState;
