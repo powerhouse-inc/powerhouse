a38df7fde6b1ff3805608ba5a950b5227b8c7a5e
Frank <frank.pfeift@protonmail.com>
2025-10-01 12:16:07 +0200

    fix(reactor-api): added auth export

--- packages/reactor-api/index.ts
+++ packages/reactor-api/index.ts
@@ -3,5 +3,7 @@ export * from "./src/graphql/graphql-manager.js";
 export * from "./src/graphql/index.js";
 export * from "./src/packages/package-manager.js";
 export * from "./src/server.js";
+export * from "./src/services/auth.service.js";
+export * from "./src/sync/utils.js";
 export * from "./src/types.js";
 export * from "./src/utils/index.js";
