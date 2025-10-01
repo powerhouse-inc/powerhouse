71dec23a3cb2655425f9fae1c86a823e65e61f55
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-10-01 10:34:09 -0500

    refactor: prepping dual action feature flag

--- apps/switchboard/src/feature-flags.ts
+++ apps/switchboard/src/feature-flags.ts
@@ -14,3 +14,8 @@ export async function isReactorv2Enabled(): Promise<boolean> {
   const client = OpenFeature.getClient();
   return await client.getBooleanValue("FEATURE_REACTORV2_ENABLED", false);
 }
+
+export async function isDualActionCreateEnabled(): Promise<boolean> {
+  const client = OpenFeature.getClient();
+  return await client.getBooleanValue("FEATURE_DUAL_ACTION_CREATE_ENABLED", false);
+}
