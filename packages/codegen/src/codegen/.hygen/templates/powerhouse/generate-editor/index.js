ded0c91e55b6ceb343aab3dacb6ba3eaf87af9db
ryanwolhuter <dev@ryanwolhuter.com>
2025-10-02 13:04:41 +0200

    feat(codegen): fix index file code lost in merge
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/codegen/src/codegen/.hygen/templates/powerhouse/generate-editor/index.js
+++ packages/codegen/src/codegen/.hygen/templates/powerhouse/generate-editor/index.js
@@ -1,6 +1,19 @@
 // @ts-check
 module.exports = {
   params: ({ args }) => {
+    const documentTypes = args.documentTypes
+      .split(",")
+      .map((type) => type.trim())
+      .filter((type) => type !== "");
+    const documentTypesMap = JSON.parse(args.documentTypesMap);
+
+    // if this editor is for a single document type, then the boilerplate will be customized to it
+    const singleDocumentType =
+      documentTypes.length === 1 ? documentTypes[0] : undefined;
+    const documentType = singleDocumentType
+      ? { ...documentTypesMap[singleDocumentType], type: singleDocumentType }
+      : undefined;
+
     return {
       rootDir: args.rootDir,
       documentModelsDir: args.documentModelsDir,
@@ -8,8 +21,9 @@ module.exports = {
       documentTypes: args.documentTypes
         .split(",")
         .filter((type) => type !== ""),
-      documentTypesMap: JSON.parse(args.documentTypesMap),
+      documentTypesMap,
       editorId: args.editorId,
+      documentType,
     };
   },
 };
