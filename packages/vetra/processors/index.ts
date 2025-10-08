abb6d374280ac1fd342ace2e989758b4e400146d
Frank <frank.pfeift@protonmail.com>
2025-10-08 09:53:47 +0200

    feat(vetra): added read model to fetch vetra packages

--- packages/vetra/processors/index.ts
+++ packages/vetra/processors/index.ts
@@ -2,3 +2,4 @@ export * as CodegenProcessor from "./codegen/index.js";
 export { codegenProcessorFactory } from "./codegen/factory.js";
 
 export { processorFactory } from "./factory.js";
+export { VetraReadModelProcessor } from "./vetra-read-model/index.js";
---
+++ packages/vetra/processors/vetra-read-model/factory.ts
@@ -0,0 +1,37 @@
+import {
+  type ProcessorRecord,
+  type IProcessorHostModule,
+} from "document-drive";
+import { type RelationalDbProcessorFilter } from "document-drive";
+import { type PHDocumentHeader } from "document-model";
+import { VetraReadModelProcessor } from "./index.js";
+
+export const vetraReadModelProcessorFactory =
+  (module: IProcessorHostModule) =>
+  async (driveHeader: PHDocumentHeader): Promise<ProcessorRecord[]> => {
+    // Create a namespace for the processor and the provided drive id
+    const namespace = VetraReadModelProcessor.getNamespace(driveHeader.id);
+
+    // Create a namespaced db for the processor
+    const store =
+      await module.relationalDb.createNamespace<VetraReadModelProcessor>(
+        namespace,
+      );
+
+    // Create a filter for the processor
+    const filter: RelationalDbProcessorFilter = {
+      branch: ["main"],
+      documentId: ["*"],
+      documentType: ["*"],
+      scope: ["global"],
+    };
+
+    // Create the processor
+    const processor = new VetraReadModelProcessor(namespace, filter, store);
+    return [
+      {
+        processor,
+        filter,
+      },
+    ];
+  };
