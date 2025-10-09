8108872e61aeb1e2bb4d61e9f2dfdfbb8fb5a13e
ryanwolhuter <dev@ryanwolhuter.com>
2025-10-09 15:49:28 +0200

    feat(reactor-browser): remove redundant types
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/reactor-browser/src/hooks/types.ts
+++ packages/reactor-browser/src/hooks/types.ts
@@ -17,51 +17,6 @@ export type AddPHGlobalEventHandler = () => void;
 export type SetEvent<TKey extends PHGlobalKey> = CustomEvent<{
   [key in TKey]: PHGlobal[TKey] | undefined;
 }>;
-export type SetReactorEvent = SetEvent<"reactor">;
-export type SetConnectCryptoEvent = SetEvent<"connectCrypto">;
-export type SetDidEvent = SetEvent<"did">;
-export type SetRenownEvent = SetEvent<"renown">;
-export type SetLoginStatusEvent = SetEvent<"loginStatus">;
-export type SetUserEvent = SetEvent<"user">;
-export type SetProcessorManagerEvent = SetEvent<"processorManager">;
-export type SetDrivesEvent = SetEvent<"drives">;
-
-export type SetDocumentsEvent = SetEvent<"documents">;
-
-export type SetSelectedDriveIdEvent = SetEvent<"selectedDriveId">;
-
-export type SetSelectedNodeIdEvent = SetEvent<"selectedNodeId">;
-
-export type SetVetraPackagesEvent = SetEvent<"vetraPackages">;
-
-export type SetModalEvent = SetEvent<"modal">;
-
-export type SetAnalyticsDatabaseNameEvent = SetEvent<"analyticsDatabaseName">;
-
-export type SetAllowListEvent = SetEvent<"allowList">;
-
-export type SetIsSearchBarEnabledEvent = SetEvent<"isSearchBarEnabled">;
-
-export type SetIsExternalControlsEnabledEvent =
-  SetEvent<"isExternalControlsEnabled">;
-
-export type SetIsDocumentToolbarEnabledEvent =
-  SetEvent<"isDocumentToolbarEnabled">;
-export type SetIsSwitchboardLinkEnabledEvent =
-  SetEvent<"isSwitchboardLinkEnabled">;
-
-export type SetIsDragAndDropEnabledEvent = SetEvent<"isDragAndDropEnabled">;
-
-export type SetIsTimelineEnabledEvent = SetEvent<"isTimelineEnabled">;
-
-export type SetIsEditorDebugModeEnabledEvent =
-  SetEvent<"isEditorDebugModeEnabled">;
-
-export type SetIsEditorReadModeEnabledEvent =
-  SetEvent<"isEditorReadModeEnabled">;
-
-export type SetSelectedTimelineRevisionEvent =
-  SetEvent<"selectedTimelineRevision">;
 
 export type LoginStatus =
   | "initial"
--- packages/reactor-browser/src/types/global.ts
+++ packages/reactor-browser/src/types/global.ts
@@ -1,31 +1,4 @@
-import type {
-  LoginStatus,
-  SetAllowListEvent,
-  SetAnalyticsDatabaseNameEvent,
-  SetConnectCryptoEvent,
-  SetDidEvent,
-  SetDocumentsEvent,
-  SetDrivesEvent,
-  SetIsDocumentToolbarEnabledEvent,
-  SetIsDragAndDropEnabledEvent,
-  SetIsEditorDebugModeEnabledEvent,
-  SetIsEditorReadModeEnabledEvent,
-  SetIsExternalControlsEnabledEvent,
-  SetIsSearchBarEnabledEvent,
-  SetIsSwitchboardLinkEnabledEvent,
-  SetIsTimelineEnabledEvent,
-  SetLoginStatusEvent,
-  SetModalEvent,
-  SetProcessorManagerEvent,
-  SetReactorEvent,
-  SetRenownEvent,
-  SetSelectedDriveIdEvent,
-  SetSelectedNodeIdEvent,
-  SetSelectedTimelineRevisionEvent,
-  SetUserEvent,
-  SetVetraPackagesEvent,
-  VetraPackage,
-} from "@powerhousedao/reactor-browser";
+import type { LoginStatus, VetraPackage } from "@powerhousedao/reactor-browser";
 import type { DID, IConnectCrypto, IRenown, User } from "@renown/sdk";
 
 import type {
@@ -70,56 +43,5 @@ export type PHGlobalValue = PHGlobal[PHGlobalKey];
 declare global {
   interface Window {
     ph?: PHGlobal;
-  }
-
-  interface WindowEventMap {
-    "ph:setReactor": SetReactorEvent;
-    "ph:reactorUpdated": CustomEvent;
-    "ph:setConnectCrypto": SetConnectCryptoEvent;
-    "ph:connectCryptoUpdated": CustomEvent;
-    "ph:setDid": SetDidEvent;
-    "ph:didUpdated": CustomEvent;
-    "ph:setRenown": SetRenownEvent;
-    "ph:renownUpdated": CustomEvent;
-    "ph:setLoginStatus": SetLoginStatusEvent;
-    "ph:loginStatusUpdated": CustomEvent;
-    "ph:setUser": SetUserEvent;
-    "ph:userUpdated": CustomEvent;
-    "ph:setProcessorManager": SetProcessorManagerEvent;
-    "ph:processorManagerUpdated": CustomEvent;
-    "ph:setDrives": SetDrivesEvent;
-    "ph:drivesUpdated": CustomEvent;
-    "ph:setDocuments": SetDocumentsEvent;
-    "ph:documentsUpdated": CustomEvent;
-    "ph:setVetraPackages": SetVetraPackagesEvent;
-    "ph:vetraPackagesUpdated": CustomEvent;
-    "ph:setSelectedDriveId": SetSelectedDriveIdEvent;
-    "ph:selectedDriveIdUpdated": CustomEvent;
-    "ph:setSelectedNodeId": SetSelectedNodeIdEvent;
-    "ph:selectedNodeIdUpdated": CustomEvent;
-    "ph:setModal": SetModalEvent;
-    "ph:modalUpdated": CustomEvent;
-    "ph:setAnalyticsDatabaseName": SetAnalyticsDatabaseNameEvent;
-    "ph:analyticsDatabaseNameUpdated": CustomEvent;
-    "ph:setAllowList": SetAllowListEvent;
-    "ph:allowListUpdated": CustomEvent;
-    "ph:setIsSearchBarEnabled": SetIsSearchBarEnabledEvent;
-    "ph:isSearchBarEnabledUpdated": CustomEvent;
-    "ph:setIsExternalControlsEnabled": SetIsExternalControlsEnabledEvent;
-    "ph:isExternalControlsEnabledUpdated": CustomEvent;
-    "ph:setIsDocumentToolbarEnabled": SetIsDocumentToolbarEnabledEvent;
-    "ph:isDocumentToolbarEnabledUpdated": CustomEvent;
-    "ph:setIsSwitchboardLinkEnabled": SetIsSwitchboardLinkEnabledEvent;
-    "ph:isSwitchboardLinkEnabledUpdated": CustomEvent;
-    "ph:setIsDragAndDropEnabled": SetIsDragAndDropEnabledEvent;
-    "ph:isDragAndDropEnabledUpdated": CustomEvent;
-    "ph:setIsTimelineEnabled": SetIsTimelineEnabledEvent;
-    "ph:isTimelineEnabledUpdated": CustomEvent;
-    "ph:setIsEditorDebugModeEnabled": SetIsEditorDebugModeEnabledEvent;
-    "ph:isEditorDebugModeEnabledUpdated": CustomEvent;
-    "ph:setIsEditorReadModeEnabled": SetIsEditorReadModeEnabledEvent;
-    "ph:isEditorReadModeEnabledUpdated": CustomEvent;
-    "ph:setSelectedTimelineRevision": SetSelectedTimelineRevisionEvent;
-    "ph:selectedTimelineRevisionUpdated": CustomEvent;
   }
 }
