f96b03b775ad94246fdf9146f1e514815b1bba60
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-10-23 11:25:36 -0500

    chore: added code coverage to reactor

--- packages/reactor/vitest.config.ts
+++ packages/reactor/vitest.config.ts
@@ -7,5 +7,16 @@ export default defineConfig({
     alias: {
       "#": new URL("./src/", import.meta.url).pathname,
     },
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "json"],
+      exclude: [
+        "test/**",
+        "dist/**",
+        "**/*.test.ts",
+        "**/*.bench.ts",
+        "**/types.ts",
+      ],
+    },
   },
 });
