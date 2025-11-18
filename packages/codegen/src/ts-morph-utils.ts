b2c001fa0b6a5cba07105cfef8a1f15575782cec
Ryan Wolhuter <dev@ryanwolhuter.com>
2025-11-18 19:04:47 -0300

    feat(codegen): use ts morph to generate subgraphs index file (#2108)
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/codegen/src/ts-morph-utils.ts
+++ packages/codegen/src/ts-morph-utils.ts
@@ -12,6 +12,73 @@ import {
   VariableDeclarationKind,
 } from "ts-morph";
 
+type MakeSubgraphsIndexFileArgs = { projectDir: string };
+export function makeSubgraphsIndexFile({
+  projectDir,
+}: MakeSubgraphsIndexFileArgs) {
+  // use the local tsconfig.json file for a given project
+  const tsConfigFilePath = path.join(projectDir, "tsconfig.json");
+
+  const project = new Project({
+    tsConfigFilePath,
+    // don't add files from the tsconfig.json file, only use the ones we need
+    skipAddingFilesFromTsConfig: true,
+    // don't load library files, we only need the files we're adding
+    skipLoadingLibFiles: true,
+    // use formatting rules which match prettier
+    manipulationSettings: {
+      useTrailingCommas: true,
+      indentationText: IndentationText.TwoSpaces,
+    },
+  });
+
+  project.addSourceFilesAtPaths(`${projectDir}/subgraphs/**/*`);
+
+  const subgraphsDir = project.getDirectory(path.join(projectDir, "subgraphs"));
+  const subgraphsSubdirs = subgraphsDir?.getDirectories() ?? [];
+
+  let subgraphsIndexSourceFile = project.getSourceFile(
+    path.join(projectDir, "subgraphs/index.ts"),
+  );
+  if (!subgraphsIndexSourceFile) {
+    subgraphsIndexSourceFile = project.createSourceFile(
+      path.join(projectDir, "subgraphs/index.js"),
+      "",
+    );
+  }
+
+  for (const subgraphSubdir of subgraphsSubdirs) {
+    const subgraphIndexSourceFilePath = `${subgraphSubdir.getPath()}/index.ts`;
+    const subgraphIndexSourceFile = project.getSourceFile(
+      subgraphIndexSourceFilePath,
+    );
+    if (!subgraphIndexSourceFile) {
+      continue;
+    }
+    const subgraphClassExport = subgraphIndexSourceFile
+      .getClasses()
+      .find((c) => c.getBaseClass()?.getText().includes("BaseSubgraph"));
+    const subgraphClassName = subgraphClassExport?.getName();
+    if (!subgraphClassName) {
+      continue;
+    }
+    const indexFileExports = subgraphsIndexSourceFile
+      .getExportDeclarations()
+      .map((e) => e.getNamespaceExport()?.getText())
+      .filter((e) => e !== undefined)
+      .join();
+    if (indexFileExports.includes(subgraphClassName)) {
+      continue;
+    }
+    subgraphsIndexSourceFile.addExportDeclaration({
+      namespaceExport: subgraphClassName,
+      moduleSpecifier: `./${subgraphSubdir.getBaseName()}/index.js`,
+    });
+  }
+
+  project.saveSync();
+}
+
 type MakeModuleFileArgs = {
   /** The root directory of the project */
   projectDir: string;
