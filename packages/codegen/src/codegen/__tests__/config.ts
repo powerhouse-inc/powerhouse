4c849ff82d5a9530cca7118f21153a20d117564f
ryanwolhuter <dev@ryanwolhuter.com>
2025-12-02 16:05:34 -0300

    feat(codegen): update test commands
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/codegen/src/codegen/__tests__/config.ts
+++ packages/codegen/src/codegen/__tests__/config.ts
@@ -1,2 +1,2 @@
 export const PURGE_AFTER_TEST = process.env.KEEP_TEST_OUTPUT !== "true";
-export const USE_LEGACY = process.env.USE_LEGACY === "true";
+export const USE_LEGACY = process.env.USE_TS_MORPH !== "true";
