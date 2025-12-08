4347307dc9eb734f5dac5f8afbf54dbb4a0d17f7
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-12-08 10:24:19 -0600

    fix: adding powerhouse drive to default options for switchboard when running with pnpm start

--- apps/switchboard/src/config.ts
+++ apps/switchboard/src/config.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { getConfig } from "@powerhousedao/config/node";
+import type { DriveInput } from "document-drive";
 const phConfig = getConfig();
 const { switchboard } = phConfig;
 interface Config {
@@ -10,6 +11,7 @@ interface Config {
   };
   port: number;
   mcp: boolean;
+  drive: DriveInput;
 }
 export const config: Config = {
   database: {
@@ -25,4 +27,18 @@ export const config: Config = {
       ? Number(process.env.PH_SWITCHBOARD_PORT)
       : (switchboard?.port ?? 4001),
   mcp: true,
+  drive: {
+    id: "powerhouse",
+    slug: "powerhouse",
+    global: {
+      name: "Powerhouse",
+      icon: "https://ipfs.io/ipfs/QmcaTDBYn8X2psGaXe7iQ6qd8q6oqHLgxvMX9yXf7f9uP7",
+    },
+    local: {
+      availableOffline: true,
+      listeners: [],
+      sharingType: "public",
+      triggers: [],
+    },
+  },
 };
