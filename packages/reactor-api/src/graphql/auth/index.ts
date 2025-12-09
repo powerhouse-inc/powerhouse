c45f47c48c4569a1d4dae4022833fb2e52c040ec
Frank <frank.pfeift@protonmail.com>
2025-12-09 15:51:55 +0100

    feat(reactor-api): added feature flag for document permission service

---
+++ packages/reactor-api/src/graphql/auth/index.ts
@@ -0,0 +1,1 @@
+export { AuthSubgraph } from "./subgraph.js";
--- packages/reactor-api/src/graphql/index.ts
+++ packages/reactor-api/src/graphql/index.ts
@@ -1,4 +1,5 @@
 export * from "./analytics-subgraph.js";
+export * from "./auth/index.js";
 export * from "./base-subgraph.js";
 export * from "./drive-subgraph.js";
 export * from "./graphql-manager.js";
