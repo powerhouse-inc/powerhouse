ad629b7baa1c2e1ef66191389917a8aeade62862
Guillermo Puente <guillermo.ps09@gmail.com>
2025-12-24 11:35:02 -0300

    test: re-generated doc-model with upgradeManifest export

--- test/versioned-documents/document-models/document-models.ts
+++ test/versioned-documents/document-models/document-models.ts
@@ -1,5 +1,9 @@
-import type { DocumentModelModule } from "document-model";
+import type { DocumentModelModule, UpgradeManifest } from "document-model";
+import { upgradeManifest as todoUpgradeManifest } from "./todo/upgrades/upgrade-manifest.js";
 import { Todo as TodoV1 } from "./todo/v1/module.js";
 import { Todo as TodoV2 } from "./todo/v2/module.js";
 
 export const documentModels: DocumentModelModule<any>[] = [TodoV1, TodoV2];
+export const upgradeManifests: UpgradeManifest<readonly number[]>[] = [
+  todoUpgradeManifest,
+];
