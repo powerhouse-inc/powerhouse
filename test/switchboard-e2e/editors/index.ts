fe00f5a35123e9e86946bb3345a0372134ed4db0
CallmeT-ty <148560082+CallmeT-ty@users.noreply.github.com>
2026-01-06 10:14:12 -0300

    feat: add end-to-end tests for Switchboard features including subgraphs and relational database processors

---
+++ test/switchboard-e2e/editors/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Editors Index
+ *
+ * This file exports all editors used in the switchboard-e2e tests.
+ * Editors are generated during global-setup.
+ */
+
+export { };
+
---
+++ test/switchboard-e2e/subgraphs/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Subgraphs Index
+ *
+ * This file exports all subgraphs used in the switchboard-e2e tests.
+ * The todo subgraph is generated during global-setup.
+ */
+
+export { };
+
