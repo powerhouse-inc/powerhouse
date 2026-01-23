bfb6fe0e84ea930832f815dbac6d619a32e5e1b2
ryanwolhuter <dev@ryanwolhuter.com>
2026-01-23 16:07:00 +0200

    fix(ph-cli): fix backwards comparison in version gte check
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/common/clis/utils.ts
+++ packages/common/clis/utils.ts
@@ -4,7 +4,7 @@ import { MINIMUM_NODE_VERSION } from "./constants.js";
 export function assertNodeVersion(nodeVersion = process.versions.node) {
   if (!nodeVersion) return;
 
-  if (gte(MINIMUM_NODE_VERSION, nodeVersion)) {
+  if (gte(nodeVersion, MINIMUM_NODE_VERSION)) {
     throw new Error(
       `Node version ${MINIMUM_NODE_VERSION} or higher is required. Current version: ${nodeVersion}`,
     );
