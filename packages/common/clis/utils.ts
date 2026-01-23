3804f3feb3fe08a39b927d709290eb51649072e7
ryanwolhuter <dev@ryanwolhuter.com>
2026-01-23 16:18:02 +0200

    fix(common): use lt instead of gte in node version check
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/common/clis/utils.ts
+++ packages/common/clis/utils.ts
@@ -1,10 +1,10 @@
-import { gte } from "semver";
+import { lt } from "semver";
 import { MINIMUM_NODE_VERSION } from "./constants.js";
 
 export function assertNodeVersion(nodeVersion = process.versions.node) {
   if (!nodeVersion) return;
 
-  if (gte(nodeVersion, MINIMUM_NODE_VERSION)) {
+  if (lt(nodeVersion, MINIMUM_NODE_VERSION)) {
     throw new Error(
       `Node version ${MINIMUM_NODE_VERSION} or higher is required. Current version: ${nodeVersion}`,
     );
