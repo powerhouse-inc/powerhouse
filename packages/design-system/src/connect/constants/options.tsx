54fa6566d46f1448458041aeb6b29ca869f6218c
Guillermo Puente <guillermo.ps09@gmail.com>
2026-01-29 17:21:55 -0300

    fix(design-system): update dropdown icons size and add drive modal position

--- packages/design-system/src/connect/constants/options.tsx
+++ packages/design-system/src/connect/constants/options.tsx
@@ -32,17 +32,17 @@ export const nodeOptions = [...normalNodeOptions, ...debugNodeOptions] as const;
 export const sharingTypeOptions = [
   {
     value: "LOCAL",
-    icon: <Icon name="Lock" />,
+    icon: <Icon name="Lock" size={16} />,
     description: "Only available to you",
   },
   {
     value: "CLOUD",
-    icon: <Icon name="People" />,
+    icon: <Icon name="People" size={16} />,
     description: "Only available to people in this drive",
   },
   {
     value: "PUBLIC",
-    icon: <Icon name="Globe" />,
+    icon: <Icon name="Globe" size={16} />,
     description: "Available to everyone",
     disabled: true,
   },
@@ -52,52 +52,52 @@ export const locationInfoByLocation = {
   CLOUD: {
     title: "Secure cloud",
     description: "End to end encryption between members.",
-    icon: <Icon name="Lock" />,
+    icon: <Icon name="Lock" size={16} />,
   },
   LOCAL: {
     title: "Local",
     description: "Private and only available to you.",
-    icon: <Icon name="Hdd" />,
+    icon: <Icon name="Hdd" size={16} />,
   },
   SWITCHBOARD: {
     title: "Switchboard",
     description: "Public and available to everyone.",
-    icon: <Icon name="Drive" />,
+    icon: <Icon name="Drive" size={16} />,
   },
 } as const;
 
 export const debugNodeOptionsMap: Record<DebugNodeOption, OptionMetadata> = {
   ["ADD_TRIGGER"]: {
     label: "Add Trigger",
-    icon: <Icon className="text-orange-900" name="Plus" />,
+    icon: <Icon className="text-orange-900" name="Plus" size={16} />,
   },
   ["REMOVE_TRIGGER"]: {
     label: "Remove Trigger",
-    icon: <Icon className="text-orange-900" name="Xmark" />,
+    icon: <Icon className="text-orange-900" name="Xmark" size={16} />,
   },
   ["ADD_INVALID_TRIGGER"]: {
     label: "Add Trigger",
-    icon: <Icon className="text-orange-900" name="Exclamation" />,
+    icon: <Icon className="text-orange-900" name="Exclamation" size={16} />,
   },
 } as const;
 
 export const normalNodeOptionsMap: Record<NormalNodeOption, OptionMetadata> = {
   DUPLICATE: {
     label: "Duplicate",
-    icon: <Icon name="FilesEarmark" />,
+    icon: <Icon name="FilesEarmark" size={16} />,
   },
   RENAME: {
     label: "Rename",
-    icon: <Icon name="Pencil" />,
+    icon: <Icon name="Pencil" size={16} />,
   },
   DELETE: {
     label: "Delete",
-    icon: <Icon name="Trash" />,
+    icon: <Icon name="Trash" size={16} />,
     className: "text-red-900",
   },
   SETTINGS: {
     label: "Settings",
-    icon: <Icon name="Gear" />,
+    icon: <Icon name="Gear" size={16} />,
   },
 } as const;
 
