e4412d6f7e2746903f2d7fb75ddcf4029e5677cb
Guillermo Puente <guillermo.ps09@gmail.com>
2026-02-05 17:08:47 -0300

    feat(reactor-api): remove SystemSubgraph and related tests, update DefaultCoreSubgraphs

--- packages/reactor-api/src/config.ts
+++ packages/reactor-api/src/config.ts
@@ -1,11 +1,7 @@
 import { AnalyticsSubgraph } from "./graphql/analytics-subgraph.js";
-import { SystemSubgraph } from "./graphql/system/index.js";
 
 export const config = {
   basePath: process.env.BASE_PATH || "/",
 };
 
-export const DefaultCoreSubgraphs = [
-  SystemSubgraph,
-  AnalyticsSubgraph,
-] as const;
+export const DefaultCoreSubgraphs = [AnalyticsSubgraph] as const;
