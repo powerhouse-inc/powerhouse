f1cb46b8a2b2b844a7a24ebdcb5b4eaa23783557
acaldas <acaldas.dev@gmail.com>
2026-02-12 13:19:50 +0000

    fix(design-system): copy assets to dist folder to enable relative path import

--- packages/design-system/src/connect/constants/icons.ts
+++ packages/design-system/src/connect/constants/icons.ts
@@ -2,15 +2,15 @@ import type { TDocumentType } from "@powerhousedao/design-system";
 import { BUDGET, DEFAULT, MAKERDAO_RWA_PORTFOLIO } from "./documents.js";
 
 const BudgetStatementImg = new URL(
-  "../../../../../assets/icons/budget.png",
+  "../../../assets/icons/budget.png",
   import.meta.url,
 ).href;
 const MakerdaoRWAPortfolioImg = new URL(
-  "../../../../../assets/icons/rwa-report.png",
+  "../../../assets/icons/rwa-report.png",
   import.meta.url,
 ).href;
 const DefaultImg = new URL(
-  "../../../../../assets/icons/template.png",
+  "../../../assets/icons/template.png",
   import.meta.url,
 ).href;
 
