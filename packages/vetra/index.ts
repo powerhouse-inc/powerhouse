49dc94650e0f1d147c204e135604c43f6ea5d712
ryanwolhuter <dev@ryanwolhuter.com>
2026-02-16 15:24:47 +0200

    fix(vetra): remove processors export
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/vetra/index.ts
+++ packages/vetra/index.ts
@@ -2,5 +2,4 @@ import type { Manifest } from "document-model";
 import manifestJson from "./powerhouse.manifest.json" with { type: "json" };
 export { documentModels } from "./document-models/document-models.js";
 export { editors } from "./editors/editors.js";
-export { processorFactory } from "./processors/factory.js";
 export const manifest: Manifest = manifestJson;
