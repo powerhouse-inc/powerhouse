ba15308e424a54db2ad55379f492399fc81b8005
ryanwolhuter <dev@ryanwolhuter.com>
2026-03-03 13:08:33 +0200

    fix: undo remove drive analytics from common
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ packages/common/drive-analytics/index.ts
@@ -0,0 +1,3 @@
+export * from "@powerhousedao/shared/processors/drive-analytics/index";
+export * from "./useDocumentAnalytics.js";
+export * from "./useDriveAnalytics.js";
---
+++ packages/common/drive-analytics/useDriveAnalytics.ts
@@ -0,0 +1,81 @@
+import type { AnalyticsQuery } from "@powerhousedao/analytics-engine-core";
+import {
+  AnalyticsGranularity,
+  AnalyticsPath,
+} from "@powerhousedao/analytics-engine-core";
+import { useAnalyticsQuery } from "@powerhousedao/reactor-browser/analytics";
+import type {
+  ActionType,
+  NodeTarget,
+} from "@powerhousedao/shared/processors/drive-analytics/types";
+import { DateTime } from "luxon";
+
+export type UseDriveAnalyticsOptions = {
+  from?: string;
+  to?: string;
+  granularity?: AnalyticsGranularity;
+  levelOfDetail?: {
+    drive?: number;
+    operation?: number;
+    target?: number;
+    actionType?: number;
+  };
+  filters?: {
+    driveId?: string[];
+    operation?: string[];
+    target?: NodeTarget[];
+    actionType?: ActionType[];
+  };
+};
+
+export const useDriveAnalytics = (options: UseDriveAnalyticsOptions) => {
+  const start = options.from
+    ? DateTime.fromISO(options.from)
+    : DateTime.now().startOf("day");
+
+  const end = options.to
+    ? DateTime.fromISO(options.to)
+    : DateTime.now().endOf("day");
+
+  const granularity = options.granularity ?? AnalyticsGranularity.Daily;
+
+  const lod = options.levelOfDetail ?? {
+    drive: 1,
+  };
+
+  const selectDrives = options.filters?.driveId?.map((driveId) =>
+    AnalyticsPath.fromString(`ph/drive/${driveId}`),
+  );
+
+  const operations = options.filters?.operation?.map((operation) =>
+    AnalyticsPath.fromString(`ph/drive/operation/${operation}`),
+  );
+
+  const targets = options.filters?.target?.map((target) =>
+    AnalyticsPath.fromString(`ph/drive/target/${target}`),
+  );
+
+  const actionTypes = options.filters?.actionType?.map((actionType) =>
+    AnalyticsPath.fromString(`ph/drive/actionType/${actionType}`),
+  );
+
+  const select = {
+    drive: selectDrives ?? [AnalyticsPath.fromString("ph/drive")],
+    ...(operations && { operation: operations }),
+    ...(targets && { target: targets }),
+    ...(actionTypes && { actionType: actionTypes }),
+  };
+
+  const config: AnalyticsQuery = {
+    start,
+    end,
+    metrics: ["DriveOperations"],
+    granularity,
+    lod,
+    select,
+  };
+
+  const result = useAnalyticsQuery(config);
+
+  return result;
+};
