0068b1ff42464a26b404df60546611245fc5ed00
ryanwolhuter <dev@ryanwolhuter.com>
2026-03-16 16:29:07 +0200

    chore: comment
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- apps/connect/start-connect.tsx
+++ apps/connect/start-connect.tsx
@@ -3,6 +3,14 @@ import type { DocumentModelLib } from "document-model";
 import { createRoot } from "react-dom/client";
 import { AppLoader } from "./src/components/index.js";
 
+/* Starts your local dev server for Connect.
+ *
+ * Call this function with the export of your project's index.ts file in your main.tsx file like this:
+ *
+ * // main.tsx
+ * import * as localPackage from "./index.js";
+ * startConnect(localPackage);
+ */
 export function startConnect(localPackage: Partial<DocumentModelLib>) {
   if (!window.ph) {
     window.ph = {};
