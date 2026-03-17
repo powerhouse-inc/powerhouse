464aaed781d62bb9f6abbc3f635ee6d77f7d9301
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-03-17 08:52:38 -0500

    fix(codegen): use relative imports instead of barrel imports in codegen

--- packages/codegen/src/templates/document-model/module.ts
+++ packages/codegen/src/templates/document-model/module.ts
@@ -2,13 +2,11 @@ import { ts } from "@tmpl/core";
 
 type DocumentModelModuleFileTemplateArgs = {
   phStateName: string;
-  versionedDocumentModelPackageImportPath: string;
   pascalCaseDocumentType: string;
   version: number;
 };
 export function documentModelModuleFileTemplate({
   phStateName,
-  versionedDocumentModelPackageImportPath,
   pascalCaseDocumentType,
   version,
 }: DocumentModelModuleFileTemplateArgs) {
@@ -16,13 +14,11 @@ export function documentModelModuleFileTemplate({
   import type { DocumentModelModule } from "document-model";
   import { createState } from "document-model";
   import { defaultBaseState } from "document-model/core";
-  import type { ${phStateName} } from "${versionedDocumentModelPackageImportPath}";
-  import {
-    actions,
-    documentModel,
-    reducer,
-    utils,
-  } from "${versionedDocumentModelPackageImportPath}";
+  import type { ${phStateName} } from "./gen/types.js";
+  import { documentModel } from "./gen/document-model.js";
+  import { reducer } from "./gen/reducer.js";
+  import { actions } from "./actions.js";
+  import { utils } from "./utils.js";
 
   /** Document model module for the ${pascalCaseDocumentType} document type */
   export const ${pascalCaseDocumentType}: DocumentModelModule<${phStateName}> = {
