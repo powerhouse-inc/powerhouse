2d4dd5ffdf444e450493d94131d6c258cce1af23
Guillermo Puente <guillermo.ps09@gmail.com>
2026-03-18 17:04:43 -0300

    fix(codegen): namespace module action exports to prevent duplicate identifier collision with baseActions

--- packages/codegen/src/templates/document-model/gen/creators.ts
+++ packages/codegen/src/templates/document-model/gen/creators.ts
@@ -3,20 +3,31 @@ import { ts } from "@tmpl/core";
 import { camelCase, kebabCase } from "change-case";
 import type { ModuleSpecification } from "@powerhousedao/shared/document-model";
 
-function buildModuleCreatorsExport(module: ModuleSpecification) {
+function buildModuleCreatorsExport(
+  module: ModuleSpecification,
+  camelCaseDocumentType: string,
+) {
   const kebabCaseModuleName = kebabCase(module.name);
   const camelCaseModuleName = camelCase(module.name);
+  const namespaceName = `${camelCaseDocumentType}${camelCaseModuleName.charAt(0).toUpperCase()}${camelCaseModuleName.slice(1)}Actions`;
   const moduleCreatorsExport = `export * from "./${kebabCaseModuleName}/creators.js";`;
-  const moduleCreatorsNamespaceExport = `export * as ${camelCaseModuleName}Actions from "./${kebabCaseModuleName}/creators.js";`;
+  const moduleCreatorsNamespaceExport = `export * as ${namespaceName} from "./${kebabCaseModuleName}/creators.js";`;
 
   return [moduleCreatorsExport, moduleCreatorsNamespaceExport];
 }
-function buildCreatorsExports(modules: ModuleSpecification[]) {
-  return modules.flatMap(buildModuleCreatorsExport).join("\n");
+function buildCreatorsExports(
+  modules: ModuleSpecification[],
+  camelCaseDocumentType: string,
+) {
+  return modules
+    .flatMap((module) =>
+      buildModuleCreatorsExport(module, camelCaseDocumentType),
+    )
+    .join("\n");
 }
 export const documentModelGenCreatorsFileTemplate = (
   v: DocumentModelTemplateInputs,
 ) =>
   ts`
-${buildCreatorsExports(v.modules)}
+${buildCreatorsExports(v.modules, v.camelCaseDocumentType)}
 `.raw;
