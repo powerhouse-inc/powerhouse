88f2f8d6d9d1ead737a2308e0bb24ab5b74d9e05
ryanwolhuter <dev@ryanwolhuter.com>
2026-03-23 13:23:04 +0200

    fix: reactor hypercore tests
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/reactor-hypercore/vitest.config.ts
+++ packages/reactor-hypercore/vitest.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@powerhousedao/reactor": new URL(
-        "../reactor/src/index.ts",
-        import.meta.url,
-      ).pathname,
-    },
-  },
   test: {
     include: ["test/**/*.test.ts"],
     globals: true,
