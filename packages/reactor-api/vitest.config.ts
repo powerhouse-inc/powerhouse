f3fbfbb90395bcb16a837b36b7f144070450582a
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-03-27 11:30:02 -0500

    refactor: top level pnpm test and necessary fixes

--- packages/reactor-api/vitest.config.ts
+++ packages/reactor-api/vitest.config.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
@@ -28,20 +29,16 @@ export default defineConfig({
       name: "graphql-path-resolver",
       resolveId(source, importer) {
         if (source.endsWith(".graphql")) {
-          // Resolve the path relative to the the file that imports the .graphql file
-          return resolve(
-            dirname(importer || ""),
-            `${source.startsWith("/") ? `.${source}` : source}.graphql`,
-          );
+          return resolve(dirname(importer || ""), source);
         }
-        return null; // Let other resolvers handle other imports
+        return null;
       },
       load(id) {
         if (id.endsWith(".graphql")) {
-          // Return the file path as a string, which Vite will use as the resolved module
-          return `export default ${JSON.stringify(id)}`;
+          const content = readFileSync(id, "utf-8");
+          return `export default ${JSON.stringify(content)}`;
         }
-        return null; // Let other loaders handle other files
+        return null;
       },
     },
   ],
