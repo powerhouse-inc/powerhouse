6848e119b3a3261af4fbd5d99ea01d4a51172823
Frank <frank.pfeift@protonmail.com>
2026-03-30 15:17:00 +0200

    feat(docker): use switchboard bin directly in entrypoint

--- docker/switchboard-entrypoint.sh
+++ docker/switchboard-entrypoint.sh
@@ -3,8 +3,9 @@ set -e
 
 if [ -n "$PH_REACTOR_DATABASE_URL" ] && [ "$SKIP_DB_MIGRATIONS" != "true" ]; then
     echo "[entrypoint] Running migrations..."
-    ph migrate
+    node node_modules/@powerhousedao/switchboard/dist/src/migrate.js
 fi
 
 echo "[entrypoint] Starting switchboard on port ${PORT:-3000}..."
-exec ph switchboard --port ${PORT:-3000}
+export PH_SWITCHBOARD_PORT="${PORT:-3000}"
+exec node node_modules/@powerhousedao/switchboard/dist/src/index.js
