26e9c5b8143a292ed30af204603c772743dbcddb
acaldas <acaldas@powerhouse.inc>
2026-04-01 17:17:59 +0100

    fix(codegen): fixed type on package.json boilerplate

--- packages/codegen/src/templates/boilerplate/package.json.ts
+++ packages/codegen/src/templates/boilerplate/package.json.ts
@@ -73,7 +73,7 @@ const devDependenciesTemplate = (versionedDevDependencies: string[]) =>
   json`
   ${versionedDevDependencies.join(",\n")},
   "@electric-sql/pglite": "0.3.15",
-  "@electric-sql/pglite-tools": 0.2.20",
+  "@electric-sql/pglite-tools": "0.2.20",
   "@eslint/js": "^9.38.0",
   "@tailwindcss/cli": "^4.1.18",
   "@types/node": "^24.9.2",
