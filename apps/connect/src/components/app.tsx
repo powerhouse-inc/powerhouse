97466944ac9677c99481db3bbb804a9e57cf8d0b
acaldas <acaldas@powerhouse.inc>
2026-04-10 09:00:59 +0100

    fix(design-system,connect): cleanup dependencies

--- apps/connect/src/components/app.tsx
+++ apps/connect/src/components/app.tsx
@@ -5,10 +5,7 @@ import {
   DocumentEditorDebugTools,
   serviceWorkerManager,
 } from "@powerhousedao/connect/utils";
-import {
-  ToastContainer,
-  WagmiContext,
-} from "@powerhousedao/design-system/connect";
+import { ToastContainer } from "@powerhousedao/design-system/connect";
 import { useEffect } from "react";
 import { PackageInstallPrompt } from "./package-install-prompt.js";
 export const App = () => {
@@ -36,12 +33,10 @@ export const App = () => {
 
   return (
     <SentryProvider>
-      <WagmiContext>
-        <ToastContainer position="bottom-right" containerId="connect" />
-        <Router />
-        <PackageInstallPrompt />
-        <Analytics />
-      </WagmiContext>
+      <ToastContainer position="bottom-right" containerId="connect" />
+      <Router />
+      <PackageInstallPrompt />
+      <Analytics />
     </SentryProvider>
   );
 };
--- packages/design-system/src/connect/components/sidebar/sidebar.stories.tsx
+++ packages/design-system/src/connect/components/sidebar/sidebar.stories.tsx
@@ -3,7 +3,6 @@ import type { DocumentDriveDocument } from "@powerhousedao/shared/document-drive
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import type { ComponentPropsWithoutRef } from "react";
-import { WagmiContext } from "../../context/WagmiContext.js";
 import { ConnectTooltipProvider } from "../tooltip/tooltip.js";
 import { SidebarItem } from "./sidebar-item.js";
 import { ConnectSidebar } from "./sidebar.js";
@@ -26,28 +25,26 @@ const user = {
 
 const Wrapper = (args: Args) => {
   return (
-    <WagmiContext>
-      <ConnectTooltipProvider>
-        <div className="relative h-screen">
-          <ConnectSidebar
-            {...args}
-            headerContent={
-              <div className="flex h-full items-center">
-                <Icon name="ConnectSmall" size={24} />
-              </div>
-            }
-          >
-            <SidebarItem title="My Local Drive" />
-            <SidebarItem title="Shared Documents" />
-            <SidebarItem title="Project Files" active={true} />
-            <SidebarItem title="Archive" />
-            <SidebarItem title="Templates" />
-            <SidebarItem title="Backups" />
-            <SidebarItem title="Settings" />
-          </ConnectSidebar>
-        </div>
-      </ConnectTooltipProvider>
-    </WagmiContext>
+    <ConnectTooltipProvider>
+      <div className="relative h-screen">
+        <ConnectSidebar
+          {...args}
+          headerContent={
+            <div className="flex h-full items-center">
+              <Icon name="ConnectSmall" size={24} />
+            </div>
+          }
+        >
+          <SidebarItem title="My Local Drive" />
+          <SidebarItem title="Shared Documents" />
+          <SidebarItem title="Project Files" active={true} />
+          <SidebarItem title="Archive" />
+          <SidebarItem title="Templates" />
+          <SidebarItem title="Backups" />
+          <SidebarItem title="Settings" />
+        </ConnectSidebar>
+      </div>
+    </ConnectTooltipProvider>
   );
 };
 
