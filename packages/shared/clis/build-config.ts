27f0ea69c6f3b27d98a089e48237df3aa90d0480
Guillermo Puente <guillermo.ps09@gmail.com>
2026-04-20 14:42:58 -0400

    feat: emit editor assets as hashed files via resolveNewUrlToAsset

--- packages/shared/clis/build-config.ts
+++ packages/shared/clis/build-config.ts
@@ -77,6 +77,9 @@ export const browserBuildConfig: InlineConfig = {
   dts,
   sourcemap,
   plugins: esmExternalRequirePlugin({ external: reactExternals }),
+  inputOptions: {
+    experimental: { resolveNewUrlToAsset: true },
+  },
 };
 
 export const nodeBuildConfig: InlineConfig = {
