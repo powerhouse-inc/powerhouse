1b2403f48b9a0f15555057255d25a33af7a5bba3
ryanwolhuter <dev@ryanwolhuter.com>
2026-04-22 12:09:19 +0200

    fix: remove unused cli args
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/shared/clis/args/migrate.ts
+++ packages/shared/clis/args/migrate.ts
@@ -1,4 +1,4 @@
-import { boolean, flag, option, optional, string } from "cmd-ts";
+import { option, string } from "cmd-ts";
 import { debugArgs } from "./common.js";
 
 export const migrateArgs = {
@@ -10,23 +10,6 @@ export const migrateArgs = {
       "The version to migrate to. Accepts a valid semver version or `staging`, `dev`, `latest`.",
     defaultValue: () => "latest" as const,
     defaultValueIsSerializable: true,
-  }),
-  dryRun: flag({
-    long: "dry-run",
-    type: optional(boolean),
-    description: "Run the migration without writing any files.",
-  }),
-  safe: flag({
-    long: "safe",
-    type: optional(boolean),
-    description:
-      "Run the migration and only update generated files. Does not make changes to non-generated files.",
-  }),
-  force: flag({
-    long: "force",
-    type: optional(boolean),
-    description:
-      "Run the migration even if your git working tree is not clean.",
   }),
   ...debugArgs,
 };
