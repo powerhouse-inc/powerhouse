248fc1e92d4eefb56ba0a2f22d1b7818c86728b7
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-04-29 16:32:44 -0500

    feat(reactor-api): added system subgraph which returns version and hash information

--- packages/reactor-api/src/config.ts
+++ packages/reactor-api/src/config.ts
@@ -1,7 +1,11 @@
 import { AnalyticsSubgraph } from "./graphql/analytics-subgraph.js";
+import { SystemSubgraph } from "./graphql/system/subgraph.js";
 
 export const config = {
   basePath: process.env.BASE_PATH || "/",
 };
 
-export const DefaultCoreSubgraphs = [AnalyticsSubgraph] as const;
+export const DefaultCoreSubgraphs = [
+  AnalyticsSubgraph,
+  SystemSubgraph,
+] as const;
