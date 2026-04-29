8485b54be2148736935b09fa804236475d18d032
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-04-29 09:45:19 -0500

    fix(reactor-attachments): fix the tsdown config

--- packages/reactor-attachments/tsdown.config.ts
+++ packages/reactor-attachments/tsdown.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./index.ts"],
   platform: "node",
+  outDir: "dist",
+  outExtensions: () => ({ js: ".js" }),
   clean: true,
   dts: true,
   sourcemap: true,
