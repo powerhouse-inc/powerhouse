e3c1c5ff0aae9ec5c9fd751011c06d59733826cf
acaldas <acaldas@powerhouse.inc>
2026-05-05 09:34:56 +0100

    chore: fix duplicate Todo type in versioned-documents

--- test/versioned-documents/document-models/todo/v1/tests/todo-operations.test.ts
+++ test/versioned-documents/document-models/todo/v1/tests/todo-operations.test.ts
@@ -61,4 +61,102 @@ describe("TodoOperationsOperations", () => {
     );
     expect(updatedDocument.operations.global[0].index).toEqual(0);
   });
+
+  it("should handle addTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(AddTodoInputSchema());
+
+    const updatedDocument = reducer(document, addTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe("ADD_TODO");
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
+
+  it("should handle removeTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(RemoveTodoInputSchema());
+
+    const updatedDocument = reducer(document, removeTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe(
+      "REMOVE_TODO",
+    );
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
+
+  it("should handle updateTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(UpdateTodoInputSchema());
+
+    const updatedDocument = reducer(document, updateTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe(
+      "UPDATE_TODO",
+    );
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
+
+  it("should handle addTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(AddTodoInputSchema());
+
+    const updatedDocument = reducer(document, addTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe("ADD_TODO");
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
+
+  it("should handle removeTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(RemoveTodoInputSchema());
+
+    const updatedDocument = reducer(document, removeTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe(
+      "REMOVE_TODO",
+    );
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
+
+  it("should handle updateTodo operation", () => {
+    const document = utils.createDocument();
+    const input = generateMock(UpdateTodoInputSchema());
+
+    const updatedDocument = reducer(document, updateTodo(input));
+
+    expect(isTodoDocument(updatedDocument)).toBe(true);
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe(
+      "UPDATE_TODO",
+    );
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+  });
 });
