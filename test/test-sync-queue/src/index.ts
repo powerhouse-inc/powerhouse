771352e087fcffc157989f4c2f15328cbbe47274
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-05-07 18:53:48 -0500

    feat: new test-sync-queue cli app that detects sync drift for large drives

---
+++ test/test-sync-queue/src/index.ts
@@ -0,0 +1,11 @@
+export { SyncDriver, type EventHandler } from "./sync-driver.js";
+export { StateWatcher } from "./state-watcher.js";
+export { EventRing } from "./event-ring.js";
+export type {
+  DriverState,
+  EventKind,
+  MailboxSnapshot,
+  ObservedEvent,
+  SyncDriverConfig,
+  WatcherConfig,
+} from "./types.js";
