da92f42a46087a8379384b18364a27c395583b52
ryanwolhuter <dev@ryanwolhuter.com>
2026-05-08 14:20:26 +0200

    chore: format
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/design-system/src/rwa/context/editor-context.tsx
+++ packages/design-system/src/rwa/context/editor-context.tsx
@@ -1,28 +1,28 @@
 import type { ReactNode } from "react";
 import {
-    createContext,
-    useCallback,
-    useContext,
-    useEffect,
-    useMemo,
-    useReducer,
-    useRef,
-    useState,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
 } from "react";
 import {
-    getActionOperationType,
-    getStateKeyForTableName,
-    getTableNameFor,
+  getActionOperationType,
+  getStateKeyForTableName,
+  getTableNameFor,
 } from "../constants/table.js";
 import type {
-    CashAsset,
-    EditorAction,
-    EditorDispatcher,
-    FixedIncome,
-    Operation,
-    RealWorldAssetsState,
-    TableItemType,
-    TableName,
+  CashAsset,
+  EditorAction,
+  EditorDispatcher,
+  FixedIncome,
+  Operation,
+  RealWorldAssetsState,
+  TableItemType,
+  TableName,
 } from "../types.js";
 import { makeTableData } from "../utils/table.js";
 import { getCashAsset, getFixedIncomeAssets } from "../utils/validators.js";
