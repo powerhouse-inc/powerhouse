dbd1dd698e20b5dbf493014f80434f3a5cc001db
ryanwolhuter <dev@ryanwolhuter.com>
2026-06-15 12:06:29 +0200

    chore: fix formatting broken in migration
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- packages/codegen/src/templates/app/components/NavigationBreadcrumbs.ts
+++ packages/codegen/src/templates/app/components/NavigationBreadcrumbs.ts
@@ -1,5 +1,15 @@
 import { tsx } from "@tmpl/core";
 
 export const driveExplorerNavigationBreadcrumbsFileTemplate = () =>
-  tsx`import { Breadcrumbs } from "@powerhousedao/design-system/connect"; /** Shows the navigation breadcrumbs for selected drive or folder */ export function NavigationBreadcrumbs() return ( <div className="border-b border-border pb-3 space-y-3"> <Breadcrumbs /> </div> );`
-    .raw;
+  tsx`
+import { Breadcrumbs } from "@powerhousedao/design-system/connect";
+
+/** Shows the navigation breadcrumbs for the selected drive or folder */
+export function NavigationBreadcrumbs() {
+  return (
+    <div className="border-b border-border pb-3 space-y-3">
+      <Breadcrumbs />
+    </div>
+  );
+}
+`.raw;
