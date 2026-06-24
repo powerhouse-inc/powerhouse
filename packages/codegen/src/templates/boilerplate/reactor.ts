fafcbe5974f9c3ccac18c14bc18888656e1d2643
Benjamin Jordan <benjamin@thegoldenmule.com>
2026-06-24 13:38:08 -0500

    fix: import paths are wrong

--- packages/codegen/src/templates/boilerplate/reactor.ts
+++ packages/codegen/src/templates/boilerplate/reactor.ts
@@ -6,9 +6,9 @@ export const reactorTsTemplate = ts`
  * This file is auto-generated and updated by codegen
  */
 import type { Manifest } from "document-model";
-import manifestJson from "./powerhouse.manifest.json" with { type: "json" };
-export { documentModels } from "./document-models/document-models.js";
-export { upgradeManifests } from "./document-models/upgrade-manifests.js";
-export { processorFactory } from "./processors/factory.js";
+import manifestJson from "../powerhouse.manifest.json" with { type: "json" };
+export { documentModels } from "../document-models/document-models.js";
+export { upgradeManifests } from "../document-models/upgrade-manifests.js";
+export { processorFactory } from "../processors/factory.js";
 export const manifest: Manifest = manifestJson;
 `.raw;
